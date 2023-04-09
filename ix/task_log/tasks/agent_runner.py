92203933e4b839d0265c16251dabfa6f9dec6b38
Peter Krenesky <kreneskyp@gmail.com>
2023-04-09 05:24:09 -0700

    renaming

--- ix/task_log/tasks/agent_runner.py
+++ ix/task_log/tasks/agent_runner.py
@@ -5,7 +5,16 @@ from ix.task_log.models import TaskLogMessage
 
 
 @shared_task
-def resume_task_with_feedback(message_id: int):
+def start_agent_loop(task_id: int, n: int = 1):
+    """
+    Start agent process loop
+    """
+    process = AgentProcess(task_id=task_id)
+    process.start(n=n)
+
+
+@shared_task
+def resume_agent_loop_with_feedback(message_id: int):
     """
     Continue agent process after recieving input from a user.
     """
