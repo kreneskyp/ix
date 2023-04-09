98de10f937eb098749f5ef81e4fe1d2e8e330497
Peter Krenesky <kreneskyp@gmail.com>
2023-04-09 07:05:16 -0700

    url route to capture direct link to chat

--- ix/server/urls.py
+++ ix/server/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("task_log>/", include("ix.task_log.urls")),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    re_path(r"tasks/chat/(?:.*)", index, name="index"),
+
 ]
