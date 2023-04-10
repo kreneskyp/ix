dde57f618155ea595821fac635cbc9072e183838
Peter Krenesky <kreneskyp@gmail.com>
2023-04-10 14:01:28 -0700

    missing import

--- ix/server/settings.py
+++ ix/server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
