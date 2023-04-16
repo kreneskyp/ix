5f83c8d4725364752b5985e5b0cfc5a3e0056c6f
Peter Krenesky <kreneskyp@gmail.com>
2023-04-15 21:23:01 -0700

    fix logging for google_search

--- ix/commands/google.py
+++ ix/commands/google.py
@@ -16,7 +16,7 @@ def google_search_api(query: str) -> List[str]:
     cx_id = os.environ.get("GOOGLE_CX_ID")
     if not api_key or not cx_id:
         raise ValueError("GOOGLE_API_KEY or GOOGLE_CX_ID environment variable not set.")
-    logger.debug(f"Searching Google API: ", query)
+    logger.debug(f"Searching Google API: {query}")
     service = build("customsearch", "v1", developerKey=api_key)
 
     try:
