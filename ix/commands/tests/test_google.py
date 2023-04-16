63c4a85324889a768fe90244cfbfe2cc3ac53b46
Peter Krenesky <kreneskyp@gmail.com>
2023-04-15 18:11:40 -0700

    google search command (#12)

---
+++ ix/commands/tests/test_google.py
@@ -0,0 +1,18 @@
+import pytest
+import os
+from ix.commands.google import google_search
+
+
+class TestGoogleSearch:
+    def test_google_search_scrape(self):
+        query = "Python"
+        results = google_search(query)
+        assert len(results) == 10
+        assert all("python" in result.lower() for result in results)
+
+    @pytest.mark.skipif("not ('GOOGLE_API_KEY' in os.environ)")
+    def test_google_search_api(self):
+        query = "Python"
+        results = google_search(query)
+        assert len(results) > 0
+        assert all("python" in result.lower() for result in results)
