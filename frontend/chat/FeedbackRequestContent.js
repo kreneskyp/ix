8a178d89ccadebaa129097e745085deebeca7033
Peter Krenesky <kreneskyp@gmail.com>
2023-04-16 16:25:57 -0700

    updated styling for chat components

--- frontend/chat/FeedbackRequestContent.js
+++ frontend/chat/FeedbackRequestContent.js
@@ -1,15 +1,21 @@
 import React from "react";
-import {Text, useColorModeValue} from "@chakra-ui/react";
+import { Text, useColorModeValue } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 
 const SystemFeedbackRequestContent = ({ content }) => {
   const labelColor = useColorModeValue("black", "blue.300");
   const textColor = useColorModeValue("gray.600", "gray.400");
 
-  return <>
-    <Text mt="4" color={labelColor}><b>I require input:</b></Text>
-    <Text mt="4" color={textColor}>{content.question}</Text>
-  </>
+  return (
+    <>
+      <Text mt="4" color={labelColor}>
+        <b>I require input:</b>
+      </Text>
+      <Text mt="4" color={textColor}>
+        {content.question}
+      </Text>
+    </>
+  );
 };
 
 SystemFeedbackRequestContent.propTypes = {
