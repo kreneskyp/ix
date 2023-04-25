e2ef1e307fe796087f892720983c8c654d8b5536
Peter Krenesky <kreneskyp@gmail.com>
2023-04-24 17:32:04 -0700

    message_id no longer displayed for EXECUTE_ERROR

--- frontend/chat/ExecuteErrorContent.js
+++ frontend/chat/ExecuteErrorContent.js
@@ -3,7 +3,7 @@ import { Flex, Heading, Text } from "@chakra-ui/react";
 
 const ExecuteErrorContent = ({ content }) => {
   let relatedMsg;
-  if (content.message_id !== null) {
+  if (content.message_id !== undefined) {
     relatedMsg = (
       <Text mb={5}>
         Error executing command message_id={content.message_id}.
