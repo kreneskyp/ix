38cb4e0fd1fbf0eebde153e69a16fc4ffb51191d
Peter Krenesky <kreneskyp@gmail.com>
2023-04-30 19:46:37 -0700

    ThinkContent no longer echos user_input

--- frontend/chat/ThinkContent.js
+++ frontend/chat/ThinkContent.js
@@ -4,17 +4,10 @@ import PropTypes from "prop-types";
 
 const ThinkContent = ({ content }) => {
   const labelColor = useColorModeValue("black", "blue.300");
-  const textColor = useColorModeValue("gray.600", "gray.400");
-
   return (
-    <>
-      <Text mt="4" color={labelColor}>
-        <b>Thinking:</b>
-      </Text>
-      <Text mt="4" color={textColor}>
-        {content.input}
-      </Text>
-    </>
+    <Text mt="4" color={labelColor}>
+      <b>Thinking</b>
+    </Text>
   );
 };
 
