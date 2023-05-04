032055ec061be36481ae55e2783c3128dfe06c80
Peter Krenesky <kreneskyp@gmail.com>
2023-05-03 21:17:30 -0700

    AgentEditor can now be opened again from AgentCard in ChatView

--- frontend/agents/AgentDetailModal.js
+++ frontend/agents/AgentDetailModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   Modal,
@@ -13,8 +13,20 @@ import {
 import { AgentEditor } from "agents/AgentEditor";
 import { useMutation } from "react-relay/hooks";
 import { UpdateAgentMutation } from "agents/graphql/AgentMutations";
+import { useQueryLoader } from "react-relay";
+import { ChainsQuery } from "chains/graphql/ChainsQuery";
 
 export const AgentDetailModal = ({ agent, isOpen, onClose }) => {
+  const [chainsRef, loadChains] = useQueryLoader(ChainsQuery);
+  const [hasOpened, setHasOpened] = useState(false);
+
+  useEffect(() => {
+    if (!hasOpened) {
+      loadChains();
+      setHasOpened(true);
+    }
+  }, [hasOpened]);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} size={"3xl"}>
       <ModalOverlay />
@@ -22,7 +34,7 @@ export const AgentDetailModal = ({ agent, isOpen, onClose }) => {
         <ModalHeader>Agent: {agent.name}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <AgentEditor agent={agent} />
+          <AgentEditor agent={agent} chainsRef={chainsRef} />
         </ModalBody>
       </ModalContent>
     </Modal>
