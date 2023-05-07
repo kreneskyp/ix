9e598cb35b688a72356ebeb1bb7b02d8393651c8
Peter Krenesky <kreneskyp@gmail.com>
2023-05-07 11:10:01 -0700

    Chat sidebar's AgentCard has been replaced with SideBarAgentList

--- frontend/chat/ChatView.js
+++ frontend/chat/ChatView.js
@@ -1,21 +1,18 @@
 import React, { Suspense, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Center, HStack, Spinner, VStack } from "@chakra-ui/react";
+import { Box, Center, Spinner, VStack } from "@chakra-ui/react";
 
 import { TaskProvider } from "tasks/contexts";
 import TaskLogMessageStream from "task_log/TaskLogMessageStream";
 import FeedbackForm from "task_log/FeedbackInput";
-import AutonomousToggle from "chat/AutonomousToggle";
 import { Layout, LayoutContent, LayoutLeftPane } from "site/Layout";
 import { ScrollableBox } from "site/ScrollableBox";
-import RunButton from "chat/RunButton";
 import { useQueryLoader } from "react-relay";
 import { ChatByIdQuery } from "chat/graphql/ChatByIdQuery";
 import { usePreloadedQuery } from "react-relay/hooks";
-import { AgentProvider } from "agents/graphql/AgentProvider";
-import AgentCardModalButton from "agents/AgentCardModalButton";
 import SideBarPlanList from "chat/SideBarPlanList";
 import SideBarArtifactList from "chat/sidebar/SideBarArtifactList";
+import SideBarAgentList from "chat/sidebar/SideBarAgentList";
 
 export const ChatContentShim = ({ queryRef }) => {
   const { chat } = usePreloadedQuery(ChatByIdQuery, queryRef);
@@ -30,23 +27,12 @@ export const ChatContentShim = ({ queryRef }) => {
           </TaskProvider>
         </Suspense>
       </ScrollableBox>
-      <Center w="100%" p={4} boxShadow="0px -1px 4px rgba(0, 0, 0, 0.1)">
+      <Center w="100%" p={4} mb={4} boxShadow="0px -1px 4px rgba(0, 0, 0, 0.1)">
         {/* Bottom aligned section */}
         <Box mr={10}>
           <TaskProvider taskId={moderatorTask.id}>
             <FeedbackForm />
           </TaskProvider>
-        </Box>
-        {/* nest another provider here so refresh does not affect the whole view */}
-        <Box width={100}>
-          <Suspense>
-            <TaskProvider taskId={moderatorTask.id}>
-              <HStack>
-                <RunButton />
-                <AutonomousToggle />
-              </HStack>
-            </TaskProvider>
-          </Suspense>
         </Box>
       </Center>
     </>
@@ -60,9 +46,7 @@ export const ChatLeftPaneShim = ({ queryRef }) => {
   return (
     <Suspense>
       <VStack spacing={4} align="stretch">
-        <AgentProvider agentId={chat.lead.id}>
-          <AgentCardModalButton />
-        </AgentProvider>
+        <SideBarAgentList queryRef={queryRef} />
         <SideBarPlanList queryRef={queryRef} />
         <SideBarArtifactList queryRef={queryRef} />
       </VStack>
---
+++ frontend/chat/sidebar/SideBarAgentList.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { HStack, VStack, Text, Box, useDisclosure } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { usePreloadedQuery } from "react-relay/hooks";
+import { ChatByIdQuery } from "chat/graphql/ChatByIdQuery";
+import AssistantAvatar from "chat/AssistantAvatar";
+import AgentDetailModalButton from "agents/AgentDetailModalButton";
+
+const AgentListItem = ({ agent }) => {
+  const { colorMode } = useColorMode();
+  console.log(agent);
+  return (
+    <AgentDetailModalButton agent={agent} width="100%">
+      <Box
+        bg="transparent"
+        _hover={{
+          cursor: "pointer",
+        }}
+        width="100%"
+      >
+        <HStack>
+          <AssistantAvatar agent={agent} /> <Text>{agent.name}</Text>
+        </HStack>
+      </Box>
+    </AgentDetailModalButton>
+  );
+};
+
+const SideBarAgentList = ({ queryRef }) => {
+  const { chat } = usePreloadedQuery(ChatByIdQuery, queryRef);
+  const lead = chat.lead;
+  const agents = chat.agents;
+
+  return (
+    <Box justify="left" p={3} borderRadius={5} bg="gray.800" color="gray.300">
+      <VStack spacing={1}>
+        <AgentListItem agent={lead} />
+        {agents?.map((agent, i) => (
+          <AgentListItem key={i} agent={agent} />
+        ))}
+      </VStack>
+    </Box>
+  );
+};
+
+export default SideBarAgentList;
