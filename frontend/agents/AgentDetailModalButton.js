65afb8a5371d195855db1fc29d23c757c615fae1
Peter Krenesky <kreneskyp@gmail.com>
2023-05-07 11:07:06 -0700

    AgentCardModalButton is now a reusable AgentDetailModalButton

--- frontend/agents/AgentDetailModalButton.js
+++ frontend/agents/AgentDetailModalButton.js
@@ -1,25 +1,18 @@
 import React from "react";
 import { Box, useDisclosure } from "@chakra-ui/react";
-import { useAgent } from "agents/graphql/AgentProvider";
 import { AgentDetailModal } from "agents/AgentDetailModal";
-import AgentCard from "agents/AgentCard";
 
-const AgentCardModalButton = () => {
-  const { agent } = useAgent();
+export const AgentDetailModalButton = ({ agent, children, ...props }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-
-  if (agent == null) {
-    return null;
-  }
 
   return (
     <>
       <AgentDetailModal agent={agent} isOpen={isOpen} onClose={onClose} />
-      <Box onClick={onOpen}>
-        <AgentCard />
+      <Box onClick={onOpen} {...props}>
+        {children}
       </Box>
     </>
   );
 };
 
-export default AgentCardModalButton;
+export default AgentDetailModalButton;
