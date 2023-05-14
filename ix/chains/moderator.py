ebe203fb17381010058dbd78bb208da9b38e61b5
Peter Krenesky <kreneskyp@gmail.com>
2023-05-13 18:33:23 -0700

    ChatModerator chain now functional, includes tests

--- ix/chains/moderator.py
+++ ix/chains/moderator.py
@@ -1,23 +1,14 @@
 import logging
 from typing import Dict, Any, List
 
-from langchain import LLMChain
 from langchain.chains.base import Chain
-from langchain.prompts import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import BaseLanguageModel
 
 from ix.agents.callback_manager import IxCallbackManager
 from ix.agents.llm import load_llm
-from ix.agents.models import Agent
 from ix.chains.json import parse_json
+from ix.chains.llm_chain import LLMChain
 from ix.chat.models import Chat
-from ix.task_log.models import Task
 from ix.task_log.tasks.agent_runner import start_agent_loop
-
 
 logger = logging.getLogger(__name__)
 
@@ -46,27 +37,16 @@ INSTRUCTION:
 """
 
 
-class LLMChooseAgent(LLMChain):
-    """Chain that selects an agent using the descriptions of the agents."""
-
-    output_key = "agent"
-
-    @property
-    def input_keys(self) -> List[str]:
-        """Input keys this chain expects."""
-        return ["user_input"]
-
-    @classmethod
-    def from_llm(cls, llm: BaseLanguageModel, verbose: bool = True) -> LLMChain:
-        system_message_prompt = SystemMessagePromptTemplate.from_template(
-            MODERATOR_PROMPT
-        )
-        human_template = "{user_input}"
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-        prompt = ChatPromptTemplate.from_messages(
-            [system_message_prompt, human_message_prompt]
-        )
-        return cls(prompt=prompt, llm=llm, verbose=verbose)
+LLM_CHOOSE_AGENT_CONFIG = {
+    "messages": [
+        {
+            "role": "assistant",
+            "template": MODERATOR_PROMPT,
+            "input_variables": ["agents"],
+        },
+        {"role": "user", "template": "{user_input}", "input_variables": ["user_input"]},
+    ]
+}
 
 
 class ChatModerator(Chain):
@@ -103,43 +83,37 @@ class ChatModerator(Chain):
         """Input keys this chain expects."""
         return ["user_input", "chat_id"]
 
-    def agent_prompt(self, chat_id: str) -> str:
+    def agent_prompt(self, chat: Chat) -> str:
         """build prompt for configured tools"""
-        chat = Chat.objects.get(id=chat_id)
         lines = []
         for i, agent in enumerate(chat.agents.all()):
             lines.append(f"{i}. {agent.alias}: {agent.purpose}")
         return "\n".join(lines)
 
-    def delegate_to_agent(self, agent_id, user_input):
-        task = self.callback_manager.task
-        agent = Agent.objects.get(id=agent_id)
-        subtask = Task.objects.create(
-            parent=task,
-            name="subtask: ",
-            agent_id=agent_id,
-            autonomous=task.autonomous,
-        )
-        start_agent_loop.delay(
-            task_id=subtask.id, chain_id=agent.chain_id, user_input=user_input
-        )
-        return subtask.id
-
     def _call(self, inputs: Dict[str, str]) -> Dict[str, str]:
         # 1. select agent
         user_input = inputs["user_input"]
         chat_id = inputs["chat_id"]
+        chat = Chat.objects.get(id=chat_id)
         logger.debug(f"Routing user_input={user_input}")
         response = self.selection_chain.run(
-            user_input=user_input, agents=self.agent_prompt(chat_id)
+            user_input=user_input, agents=self.agent_prompt(chat)
         )
         logger.debug(f"Moderator returned response={response}")
-        response_data = parse_json(response)
+        response_data = parse_json(response, output_key="agent")
 
         # 1. delegate to the agent
-        agent_name = response_data.pop("agent")
-        task_id = self.delegate_to_agent(agent_name, inputs, response_data)
-        return {"task_id": task_id}
+        # TODO parse_json maps the whole return string to the agent key
+        alias = response_data["agent"]["agent"]
+        agent = chat.agents.get(alias=alias)
+        subtask = chat.task.delegate_to_agent(agent)
+        logger.error(
+            f"Delegated to agent={agent.alias} task={subtask.id} input={inputs}"
+        )
+        start_agent_loop.delay(
+            task_id=str(subtask.id), chain_id=str(agent.chain_id), inputs=inputs
+        )
+        return {"task_id": str(subtask.id)}
 
     async def _acall(self, inputs: Dict[str, str]) -> Dict[str, str]:
         pass
@@ -150,7 +124,9 @@ class ChatModerator(Chain):
         llm_config = config["llm"]
         llm = load_llm(llm_config, callback_manager)
 
-        chooser = LLMChooseAgent.from_llm(llm=llm)
+        chooser_config = LLM_CHOOSE_AGENT_CONFIG.copy()
+        chooser_config["llm"] = llm
+        chooser = LLMChain.from_config(chooser_config, callback_manager)
         chooser.callback_manager = callback_manager
 
         instance = cls(
---
+++ ix/chains/tests/test_moderator.py
@@ -0,0 +1,75 @@
+from uuid import UUID
+
+import pytest
+from ix.agents.callback_manager import IxCallbackManager
+from ix.chains.llm_chain import LLMChain
+from ix.chains.moderator import ChatModerator
+from ix.task_log.tests.fake import fake_agent, fake_chat
+
+
+@pytest.mark.django_db
+class TestChatModerator:
+    @pytest.fixture()
+    def chat(self):
+        chat = fake_chat()
+        fake_agent_1 = fake_agent(
+            name="agent 1", alias="agent_1", purpose="to test selections"
+        )
+        fake_agent_2 = fake_agent(
+            name="agent 2", alias="agent_2", purpose="to test selections"
+        )
+        chat.agents.set([fake_agent_1, fake_agent_2])
+        callback_manager = IxCallbackManager(chat.task)
+
+        config = {
+            "llm": {
+                "class_path": "langchain.chat_models.openai.ChatOpenAI",
+                "config": {"request_timeout": 120, "temperature": 0.2, "verbose": True},
+            }
+        }
+
+        yield {
+            "chat": chat,
+            "fake_agent_1": fake_agent_1,
+            "fake_agent_2": fake_agent_2,
+            "instance": ChatModerator.from_config(
+                config, callback_manager=callback_manager
+            ),
+        }
+
+    def test_agent_prompt(self, chat):
+        """Test that the agent prompt is formatted correctly"""
+
+        chat_moderator = chat["instance"]
+        agent_prompt = chat_moderator.agent_prompt(chat["chat"])
+
+        assert (
+            agent_prompt
+            == """0. agent_1: to test selections\n1. agent_2: to test selections"""
+        )
+
+    def test_call(self, chat, mock_openai):
+        mock_openai.return_value = '{"agent": "agent_1"}'
+
+        chat_moderator = chat["instance"]
+        result = chat_moderator._call(
+            {"user_input": "say hello to agent 1", "chat_id": str(chat["chat"].id)}
+        )
+
+        assert "task_id" in result
+        assert UUID(result["task_id"])
+
+    def test_from_config(self, chat):
+        """Test that the ChatModerator is created with the correct dependencies from config"""
+        callback_manager = IxCallbackManager(chat["chat"].task)
+        config = {
+            "llm": {
+                "class_path": "langchain.chat_models.openai.ChatOpenAI",
+                "config": {"request_timeout": 60, "temperature": 0.2, "verbose": True},
+            }
+        }
+
+        chat_moderator = ChatModerator.from_config(config, callback_manager)
+
+        assert isinstance(chat_moderator.selection_chain, LLMChain)
+        assert chat_moderator.callback_manager == callback_manager
