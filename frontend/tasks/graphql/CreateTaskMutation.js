c0886735f3c3ddc5e786d075521725690fda414b
Peter Krenesky <kreneskyp@gmail.com>
2023-05-13 18:09:47 -0700

    remove goals from more queries

--- frontend/tasks/graphql/CreateTaskMutation.js
+++ frontend/tasks/graphql/CreateTaskMutation.js
@@ -11,9 +11,6 @@ export const CreateTaskMutation = graphql`
           name
           purpose
         }
-        goals {
-          description
-        }
       }
     }
   }
--- frontend/tasks/graphql/TasksQuery.js
+++ frontend/tasks/graphql/TasksQuery.js
@@ -9,10 +9,6 @@ export const TasksQuery = graphql`
       createdAt
       isComplete
       completeAt
-      goals {
-        description
-        complete
-      }
       agent {
         id
         name
