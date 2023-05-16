8f15c0711d6df929e3cd84671ea5a3cfb9e6081c
Peter Krenesky <kreneskyp@gmail.com>
2023-05-15 17:18:09 -0700

    connect ChatMessageSubscription to TaskLogMessage save

---
+++ ix/chat/apps.py
@@ -0,0 +1,16 @@
+# your_app/apps.py
+from django.apps import AppConfig
+
+
+class ChatAppConfig(AppConfig):
+    name = "ix.chat"
+
+    def ready(self):
+        # Connect the signal
+        from ix.schema.subscriptions import ChatMessageSubscription
+        from ix.task_log.models import TaskLogMessage
+        from django.db.models.signals import post_save
+
+        post_save.connect(
+            ChatMessageSubscription.new_task_log_message, sender=TaskLogMessage
+        )
