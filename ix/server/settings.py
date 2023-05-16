aad149a432f100e31aa58bab2a6d588b280f3a55
Peter Krenesky <kreneskyp@gmail.com>
2023-05-15 17:16:52 -0700

    moving redis cache to separate database from channels

--- ix/server/settings.py
+++ ix/server/settings.py
@@ -161,8 +161,8 @@ CHANNEL_LAYERS = {
 }
 
 # Celery configuration
-CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/2"
+CELERY_RESULT_BACKEND = "redis://redis:6379/2"
 
 CACHES = {
     "default": {
