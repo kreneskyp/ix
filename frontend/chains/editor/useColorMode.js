620a861e41e88ba0e1b4baedd693a5cd08cf1ce3
Peter Krenesky <kreneskyp@gmail.com>
2023-05-19 12:03:19 -0700

    Add hook that returns colors for chain editor

---
+++ frontend/chains/editor/useColorMode.js
@@ -0,0 +1,35 @@
+import { useColorMode } from "@chakra-ui/color-mode";
+
+export const useEditorColorMode = () => {
+  const { colorMode } = useColorMode();
+  const isLight = colorMode === "light";
+
+  return {
+    isLight,
+    highlightColor: isLight ? "gray.100" : "gray.100",
+    color: isLight ? "gray.800" : "gray.100",
+    bg: isLight ? "gray.50" : "gray.700",
+    border: isLight ? "gray.600" : "black",
+    highlight: {
+      llm: isLight ? "red.300" : "red.300",
+      prompt: isLight ? "orange.300" : "orange.500",
+      memory: isLight ? "purple.500" : "purple.300",
+      chain: isLight ? "blue.500" : "blue.300",
+      agent: isLight ? "green.500" : "green.300",
+      tool: isLight ? "yellow.500" : "yellow.300",
+    },
+  };
+};
+
+export const useSideBarColorMode = () => {
+  const { colorMode } = useColorMode();
+  const isLight = colorMode === "light";
+
+  return {
+    isLight,
+    highlightColor: isLight ? "gray.100" : "gray.100",
+    color: isLight ? "gray.800" : "gray.100",
+    bg: isLight ? "gray.300" : "gray.700",
+    border: isLight ? "gray.600" : "black",
+  };
+};
