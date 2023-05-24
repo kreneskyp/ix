94ad7aff4c4c195342fd56a9203af669e4b429a7
Peter Krenesky <kreneskyp@gmail.com>
2023-05-23 18:53:49 -0700

    Moving memory to root of Coder so it is available to planning step

--- ix/chains/management/commands/create_coder_v1.py
+++ ix/chains/management/commands/create_coder_v1.py
@@ -86,6 +86,9 @@ CODER_SEQUENCE = {
     "class_path": "ix.chains.routing.IXSequence",
     "config": {
         "input_variables": ["user_input"],
+        "memory": {
+            "class_path": "ix.memory.artifacts.ArtifactMemory",
+        },
     },
 }
 
@@ -149,7 +152,7 @@ GENERATE_FILES_MAP = {
     "description": "Generates files for each artifact in the input list",
     "class_path": "ix.chains.routing.MapSubchain",
     "config": {
-        "input_variables": ["file_artifacts_json"],
+        "input_variables": ["file_artifacts_json", "related_artifacts"],
         "map_input": "file_artifacts_json",
         "map_input_to": "file_artifact",
         "output_key": "write_file_output",
@@ -169,14 +172,11 @@ GENERATE_FILE = {
                 "verbose": True,
             },
         },
-        "memory": {
-            "class_path": "ix.memory.artifacts.ArtifactMemory",
-        },
         "messages": [
             {
                 "role": "system",
                 "template": GENERATE_CODE_V1,
-                "input_variables": ["file_artifact"],
+                "input_variables": ["file_artifact", "related_artifacts"],
                 "partial_variables": {
                     "file_content_format": FILE_CONTENT_FORMAT,
                 },
