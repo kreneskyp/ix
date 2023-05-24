a2d4414449d17b2118e8b0764e100cd94aa10dca
Peter Krenesky <kreneskyp@gmail.com>
2023-05-23 18:59:27 -0700

    move mock content function out so other functions may be replaced with MagicMocks

--- ix/agents/tests/mock_llm.py
+++ ix/agents/tests/mock_llm.py
@@ -16,19 +16,17 @@ class MockChatOpenAI(ChatOpenAI):
     def set_return_value(self, return_value: Any):
         self.return_value = return_value
 
+    def get_mock_content(self):
+        content = self.return_value or "This is a test."
+        return {
+            "choices": [{"message": {"content": content, "role": "assistant"}}],
+            "usage": {"prompt_tokens": 5, "completion_tokens": 7, "total_tokens": 12},
+        }
+
     def completion_with_retry(self, *args, **kwargs) -> Dict[str, Any]:
         if self.raise_exception:
             raise self.raise_exception
-
-        content = self.return_value or "This is a test."
-        return {
-            "choices": [{"message": {"content": content, "role": "assistant"}}],
-            "usage": {"prompt_tokens": 5, "completion_tokens": 7, "total_tokens": 12},
-        }
+        return self.get_mock_content()
 
     async def _agenerate(self, *args, **kwargs) -> Dict[str, Any]:
-        content = self.return_value or "This is a test."
-        return {
-            "choices": [{"message": {"content": content, "role": "assistant"}}],
-            "usage": {"prompt_tokens": 5, "completion_tokens": 7, "total_tokens": 12},
-        }
+        return self.get_mock_content()
