c411730be6be8337f86cf45bb1e9f0c28d446596
Peter Krenesky <kreneskyp@gmail.com>
2023-06-19 16:44:24 -0700

    adding nodes, root node, and memor_backend styling

--- frontend/chains/editor/useColorMode.js
+++ frontend/chains/editor/useColorMode.js
@@ -10,10 +10,34 @@ export const useEditorColorMode = () => {
     color: isLight ? "gray.800" : "gray.100",
     bg: isLight ? "gray.50" : "gray.700",
     border: isLight ? "gray.600" : "black",
+    node: {
+      section: isLight
+        ? {
+            color: "gray.300",
+            bg: "blackAlpha.200",
+          }
+        : {
+            color: "gray.600",
+            bg: "blackAlpha.200",
+          },
+      root: isLight
+        ? {
+            color: "gray.200",
+            bg: "gray.700",
+        } : {
+            color: "gray.400",
+            bg: "gray.800",
+        }
+    },
+    root: {
+      color: isLight ? "gray.800" : "gray.400",
+    },
     highlight: {
+      root: isLight ? "gray.300" : "gray.900",
       llm: isLight ? "red.300" : "red.300",
       prompt: isLight ? "orange.300" : "orange.500",
       memory: isLight ? "purple.500" : "purple.300",
+      memory_backend: isLight ? "purple.500" : "purple.300",
       chain: isLight ? "blue.500" : "blue.300",
       agent: isLight ? "green.500" : "green.300",
       tool: isLight ? "yellow.500" : "yellow.300",
