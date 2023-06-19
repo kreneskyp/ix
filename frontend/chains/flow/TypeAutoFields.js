de7131346698601d37d63bfe326786fa533a5add
Peter Krenesky <kreneskyp@gmail.com>
2023-06-19 16:04:00 -0700

    Add TypeAutoFields for rendering config forms from NodeType field definitions

---
+++ frontend/chains/flow/TypeAutoFields.js
@@ -0,0 +1,160 @@
+import React, { useCallback } from "react";
+import {
+  Checkbox,
+  Flex,
+  FormLabel,
+  HStack,
+  Input,
+  Select,
+  VStack,
+} from "@chakra-ui/react";
+import SliderInput from "components/SliderInput";
+
+const getLabel = (field) => field.label || labelify(field.name);
+
+const AutoFieldCheckbox = ({ field, value, onChange }) => {
+  const handleChange = useCallback(
+    (event) => {
+      onChange(field.name, event.target.checked);
+    },
+    [field, onChange]
+  );
+
+  return (
+    <HStack width="100%">
+      <FormLabel justify="start">{getLabel(field)}</FormLabel>
+      <Checkbox isChecked={value} onChange={handleChange} />
+    </HStack>
+  );
+};
+
+const AutoFieldInput = ({ field, value, onChange }) => {
+  const handleChange = useCallback(
+    (event) => {
+      onChange(field.name, event.target.value);
+    },
+    [field, onChange]
+  );
+
+  return (
+    <Flex justifyContent="space-between" wrap="wrap" width="100%">
+      <FormLabel justify="start" whiteSpace="nowrap">
+        {getLabel(field)}
+      </FormLabel>
+      <Input
+        size="sm"
+        value={value}
+        onChange={handleChange}
+        width={field.width || 100}
+        px={1}
+        py={2}
+      />
+    </Flex>
+  );
+};
+
+const AutoFieldSlider = ({ field, value, onChange }) => {
+  const handleChange = useCallback(
+    (newValue) => {
+      onChange(field.name, newValue);
+    },
+    [field, onChange]
+  );
+
+  return (
+    <SliderInput
+      label={getLabel(field)}
+      field={field.name}
+      value={value}
+      onChange={handleChange}
+      min={field.min}
+      max={field.max}
+      step={field.step}
+    />
+  );
+};
+
+const AutoFieldSelect = ({ field, value, onChange }) => {
+  const handleChange = useCallback((event) => {
+    onChange(field.name, event.target.value);
+  });
+
+  return (
+    <Flex width="100%" justifyContent="space-between">
+      <FormLabel size="sm" justify="start">
+        {getLabel(field)}
+      </FormLabel>
+      <Select
+        size="sm"
+        onChange={handleChange}
+        width={field.width || 125}
+        value={value}
+      >
+        {field.choices?.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </Select>
+    </Flex>
+  );
+};
+
+// explicit input types
+const INPUTS = {
+  select: AutoFieldSelect,
+  checkbox: AutoFieldCheckbox,
+  slider: AutoFieldSlider,
+};
+
+// type specific default inputs
+const TYPE_INPUTS = {
+  boolean: "checkbox",
+};
+
+const DEFAULT_COMPONENT = AutoFieldInput;
+
+const labelify = (key) => {
+  return key.replace(/_/g, " ").replace(/\b\w/g, (l) => l.toUpperCase());
+};
+
+const FIELD_TYPE_DEFAULTS = {
+  boolean: false,
+  number: 0,
+  string: "",
+  list: [],
+  dict: {},
+};
+
+export const getDefaults = (nodeType) => {
+  const defaults = {};
+  nodeType.fields?.forEach(
+    (field) =>
+      (defaults[field.name] = field.default || FIELD_TYPE_DEFAULTS[field.type])
+  );
+  return defaults;
+};
+export const TypeAutoField = ({ field, config, onChange }) => {
+  // Select component based on explicit input type, field type, or default in that order.
+  const FieldComponent =
+    INPUTS[field.input || TYPE_INPUTS[field.type]] || DEFAULT_COMPONENT;
+  const value =
+    config && field.name in config ? config[field.name] : field.default;
+
+  return <FieldComponent field={field} value={value} onChange={onChange} />;
+};
+
+export const TypeAutoFields = ({ type, config, onChange }) => {
+  return (
+    <VStack spacing={1} width="100%">
+      {type.fields?.map((field) => (
+        <TypeAutoField
+          key={field.name}
+          field={field}
+          config={config}
+          onChange={onChange}
+        />
+      ))}
+    </VStack>
+  );
+};
