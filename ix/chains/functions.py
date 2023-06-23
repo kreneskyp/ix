8ce5f60dcb5d73de13b278e55bf2c8238962033e
Peter Krenesky <kreneskyp@gmail.com>
2023-06-22 21:28:03 -0700

    OpenAiFunctionParser may now optionally parse function json into an object

--- ix/chains/functions.py
+++ ix/chains/functions.py
@@ -1,5 +1,10 @@
+import json
+import logging
 from typing import Any, TypedDict, List
 from langchain.schema import BaseLLMOutputParser, Generation, T
+
+
+logger = logging.getLogger(__name__)
 
 
 class FunctionSchema(TypedDict):
@@ -17,10 +22,19 @@ class OpenAIFunctionParser(BaseLLMOutputParser):
     The function_call is returned if present, otherwise the text is returned.
     """
 
+    parse_json: bool = False
+
     def parse_result(self, result: List[Generation]) -> T:
         additional_kwargs = result[0].message.additional_kwargs
-
         if "function_call" in additional_kwargs:
-            return additional_kwargs["function_call"]
+            function_call = additional_kwargs["function_call"]
+            if self.parse_json:
+                if isinstance(function_call, str):
+                    function_call = json.loads(function_call)
+
+                if isinstance(function_call["arguments"], str):
+                    function_call["arguments"] = json.loads(function_call["arguments"])
+
+            return function_call
         else:
             return result[0].text
