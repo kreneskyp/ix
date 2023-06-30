5c46e16b280bf42ef3656f1e6f77383c5a475a80
Peter Krenesky <kreneskyp@gmail.com>
2023-06-30 07:48:40 -0700

    Celery must run in SOLO pool for workers to use asyncio.

--- bin/celery.sh
+++ bin/celery.sh
@@ -4,4 +4,8 @@ set -o errexit
 CELERY_CONCURRENCY=${CELERY_CONCURRENCY:=1}
 DJANGO_SETTINGS_MODULE=ix.server.celery_settings
 
-celery -A ix worker --loglevel=info --concurrency=${CELERY_CONCURRENCY}
+# celery runs in SOLO pool to avoid concurrency issues with asyncio
+# this means that concurrency is limited to 1, even if set to a higher
+# value. A longer term solution is likely needed to support concurrency
+# within a single celery worker.
+celery -A ix worker --loglevel=info --pool=solo --concurrency=${CELERY_CONCURRENCY}
