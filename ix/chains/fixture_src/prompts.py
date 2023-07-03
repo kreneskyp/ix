7a3502e13db97c14e2b680759b4648d8c2ca6c68
Peter Krenesky <kreneskyp@gmail.com>
2023-07-03 09:29:02 -0700

    ChatPrompt once again has default values

--- ix/chains/fixture_src/prompts.py
+++ ix/chains/fixture_src/prompts.py
@@ -1,9 +1,25 @@
 from ix.chains.fixture_src.targets import OUTPUT_PARSER_TARGET
 
+DEFAULT_MESSAGES = [
+    {
+    "role": "system",
+    "template": "",
+    "input_variables": [],
+  }
+]
+
 CHAT_PROMPT_TEMPLATE = {
     "class_path": "langchain.prompts.chat.ChatPromptTemplate",
     "type": "prompt",
     "name": "Chat Prompt Template",
     "description": "Template for constructing chat prompts from messages.",
     "connectors": [OUTPUT_PARSER_TARGET],
+    "fields": [
+        {
+            "name": "messages",
+            "type": "list",
+            "default": DEFAULT_MESSAGES,
+        }
+    ]
+
 }
