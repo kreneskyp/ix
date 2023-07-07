dc4f1783a082a2b0bee5e395f1539d8e5e754fda
Peter Krenesky <kreneskyp@gmail.com>
2023-07-07 12:51:46 -0700

    Add SyncToAsyncCall for adding asyncio support to Chain

--- ix/chains/asyncio.py
+++ ix/chains/asyncio.py
@@ -1,6 +1,9 @@
 from typing import Optional, Any, Dict
 from asgiref.sync import sync_to_async
-from langchain.callbacks.manager import AsyncCallbackManagerForToolRun, CallbackManagerForChainRun
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForChainRun,
+)
 
 
 class SyncToAsyncRun:
@@ -19,4 +22,23 @@ class SyncToAsyncRun:
     ) -> str:
         """Use the tool asynchronously."""
         result = await sync_to_async(self._run)(query, run_manager=run_manager)
+        return result
+
+
+class SyncToAsyncCall:
+    """
+    Mixin to convert a chain or tool to run asynchronously by using
+    sync_to_async to convert the _call method to a coroutine.
+
+    This doesn't provide full async support, but it does allow for
+    the chain/tool to work.
+    """
+
+    async def _acall(
+        self,
+        inputs: Dict[str, Any],
+        run_manager: Optional[CallbackManagerForChainRun] = None,
+    ) -> str:
+        """Use the tool asynchronously."""
+        result = await sync_to_async(self._call)(inputs, run_manager=run_manager)
         return result
