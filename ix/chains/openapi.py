40ffe62583a60e0325fb0a9ef4c23324757fb35c
Peter Krenesky <kreneskyp@gmail.com>
2023-07-07 17:22:03 -0700

    get_openapi_chain_async prompt updated for consistency with Ix prompts

--- ix/chains/openapi.py
+++ ix/chains/openapi.py
@@ -4,6 +4,8 @@ from langchain.chains.openai_functions.openapi import (
     get_openapi_chain,
     SimpleRequestChain,
 )
+from langchain.prompts import ChatPromptTemplate
+
 from ix.chains.asyncio import SyncToAsyncCall
 
 logger = logging.getLogger(__name__)
@@ -22,5 +24,11 @@ def get_openapi_chain_async(**kwargs):
         "langchain.chains.openai_functions.openapi.SimpleRequestChain",
         new=AsyncSimpleRequestChainRun,
     ):
+        # modified to use `user_input` for consistency with other chains
+        if "prompt" not in kwargs:
+            kwargs['prompt'] = ChatPromptTemplate.from_template(
+                "Use the provided API's to respond to this user query:\n\n{user_input}"
+            )
+
         chain = get_openapi_chain(**kwargs)
         return chain
