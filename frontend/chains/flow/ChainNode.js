ae86f953d790106a044ed2e278776c9d5218105b
Peter Krenesky <kreneskyp@gmail.com>
2023-07-16 16:15:40 -0700

    ChainNode input/output connectors are now positioned left/right side of node

--- frontend/chains/flow/ChainNode.js
+++ frontend/chains/flow/ChainNode.js
@@ -1,53 +1,38 @@
 import React from "react";
 import { Handle } from "reactflow";
-import { Box, VStack, Heading, Text, HStack } from "@chakra-ui/react";
+import { Box, VStack, Heading, Flex } from "@chakra-ui/react";
 import { TypeAutoFields } from "chains/flow/TypeAutoFields";
 import { CollapsibleSection } from "chains/flow/CollapsibleSection";
 import { NodeProperties } from "chains/flow/ConfigNode";
 
 export const ChainNode = ({ type, node, config, onFieldChange }) => {
-  const inputs = null;
-  const outputs = null;
-
   return (
     <VStack spacing={0} alignItems="stretch" fontSize="xs">
-      <Box position="relative">
-        <Handle
-          id="in"
-          type="target"
-          position="left"
-          style={{ top: "50%", transform: "translateY(-50%)" }}
-        />
-        <Heading fontSize="xs" px={2}>
-          Inputs
-        </Heading>
-      </Box>
-      <VStack alignItems="stretch">
-        {inputs?.map((input, index) => (
-          <HStack key={index} spacing="2">
-            <Text>{input}</Text>
-          </HStack>
-        ))}
-      </VStack>
-      <Box position="relative">
-        <Handle
-          id="out"
-          type="source"
-          position="right"
-          style={{ top: "50%", transform: "translateY(-50%)" }}
-        />
-        <Heading fontSize="xs" px={2}>
-          Output
-        </Heading>
-      </Box>
-      <VStack alignItems="stretch" pb={2}>
-        {outputs?.map((output, index) => (
-          <HStack key={index} spacing="2">
-            <Text>{output}</Text>
-            <Handle type="source" position="bottom" id={`output-${index}`} />
-          </HStack>
-        ))}
-      </VStack>
+      <Flex mt={1} mb={3} justify={"space-between"}>
+        <Box position="relative">
+          <Handle
+            id="in"
+            type="target"
+            position="left"
+            style={{ top: "50%", transform: "translateY(-50%)" }}
+          />
+          <Heading fontSize="xs" px={2}>
+            Inputs
+          </Heading>
+        </Box>
+
+        <Box position="relative">
+          <Handle
+            id="out"
+            type="source"
+            position="right"
+            style={{ top: "50%", transform: "translateY(-50%)" }}
+          />
+          <Heading fontSize="xs" px={2}>
+            Output
+          </Heading>
+        </Box>
+      </Flex>
       <NodeProperties type={type} />
       <CollapsibleSection title="Config">
         <TypeAutoFields type={type} config={config} onChange={onFieldChange} />
