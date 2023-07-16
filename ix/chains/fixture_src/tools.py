f8d6c734134a7ae83cee71119f0d6edc9826ed32
Peter Krenesky <kreneskyp@gmail.com>
2023-07-16 10:13:24 -0700

    Add `Chain Tool` component for converting any `Chain` into a `Tool`

--- ix/chains/fixture_src/tools.py
+++ ix/chains/fixture_src/tools.py
@@ -1,3 +1,4 @@
+from ix.chains.fixture_src.targets import CHAIN_TARGET
 from langchain import (
     GoogleSearchAPIWrapper,
     GoogleSerperAPIWrapper,
@@ -14,6 +15,13 @@ from langchain.utilities import (
 
 from ix.chains.config import NodeTypeField
 from ix.chains.fixture_src.common import VERBOSE
+
+NAME = {
+    "name": "name",
+    "type": "str",
+    "default": "",
+    "style": {"width": "100%"},
+}
 
 DESCRIPTION = {
     "name": "description",
@@ -67,6 +75,15 @@ BING_SEARCH = {
             },
         },
     ),
+}
+
+CHAIN_AS_TOOL = {
+    "class_path": "ix.chains.tools.chain_as_tool",
+    "type": "tool",
+    "name": "Chain Tool",
+    "description": "Tool that runs a chain. Any chain may be converted into a tool.",
+    "connectors": [CHAIN_TARGET],
+    "fields": [NAME, DESCRIPTION] + TOOL_BASE_FIELDS,
 }
 
 DUCK_DUCK_GO_SEARCH = {
@@ -198,6 +215,7 @@ WOLFRAM = {
 TOOLS = [
     ARXIV_SEARCH,
     BING_SEARCH,
+    CHAIN_AS_TOOL,
     DUCK_DUCK_GO_SEARCH,
     GOOGLE_SEARCH,
     GOOGLE_SERPER,
---
+++ ix/chains/tools.py
@@ -0,0 +1,13 @@
+from langchain.chains.base import Chain
+from langchain.tools import Tool, BaseTool
+
+
+def chain_as_tool(chain: Chain, name: str, description: str, **kwargs) -> BaseTool:
+    """Converts a chain into a tool."""
+    return Tool(
+        name=name,
+        description=description,
+        func=chain.run,
+        coroutine=chain.arun,
+        **kwargs
+    )
