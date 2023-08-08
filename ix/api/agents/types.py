2fbeccbee1d5e11637a5279fdc837e02a9a5cce1
Peter Krenesky <kreneskyp@gmail.com>
2023-08-07 17:44:18 -0700

    get_agents endpoint now has search and pagination

--- ix/api/agents/types.py
+++ ix/api/agents/types.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from typing import List
 from uuid import UUID
 
 from pydantic import BaseModel, Field
 
 import logging
+
+from ix.utils.graphene.pagination import QueryPage
 
 logger = logging.getLogger(__name__)
 
@@ -20,3 +23,8 @@ class Agent(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class AgentPage(QueryPage[Agent]):
+    # override objects, FastAPI isn't detecting QueryPage type
+    objects: List[Agent]
