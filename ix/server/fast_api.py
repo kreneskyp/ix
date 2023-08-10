fecc5d0cecef98326e42d14c27b5e0b63ed31454
Peter Krenesky <kreneskyp@gmail.com>
2023-08-09 19:46:44 -0700

    Adding endpoints for artifacts

--- ix/server/fast_api.py
+++ ix/server/fast_api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 
 from ix.api.agents.endpoints import router as agents_router
+from ix.api.artifacts.endpoints import router as artifacts_router
 from ix.api.chains.endpoints import router as chains_router
 from ix.api.chats.endpoints import router as chats_router
 
@@ -12,6 +13,7 @@ app = FastAPI(
 app.include_router(chains_router)
 app.include_router(agents_router)
 app.include_router(chats_router)
+app.include_router(artifacts_router)
 
 
 def custom_openapi():
