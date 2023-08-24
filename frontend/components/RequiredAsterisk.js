e444fa6d622807cc4c331c8f0ad00f8f673f3841
Peter Krenesky <kreneskyp@gmail.com>
2023-08-24 07:48:48 -0700

    Node connectors are now styled based on connection and requirement status

--- frontend/components/RequiredAsterisk.js
+++ frontend/components/RequiredAsterisk.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Text } from "@chakra-ui/react";
 import { useColorMode } from "@chakra-ui/color-mode";
 
-export const RequiredAsterisk = () => {
+export const RequiredAsterisk = ({color}) => {
   const { colorMode } = useColorMode();
-  const color = colorMode === "light" ? "red.500" : "red.200";
+  const _color = color || (colorMode === "light" ? "red.500" : "red.300");
   return (
-    <Text as="span" color={color}>
+    <Text as="span" color={_color}>
       *
     </Text>
   );
