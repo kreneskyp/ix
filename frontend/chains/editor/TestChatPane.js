01af6bcd5b80e9d05a2d91b81e5fbec12cdefa9e
Peter Krenesky <kreneskyp@gmail.com>
2023-09-04 17:54:24 -0700

    TestChatPane now shows a message when chain hasn't been laoded

--- frontend/chains/editor/TestChatPane.js
+++ frontend/chains/editor/TestChatPane.js
@@ -2,11 +2,21 @@ import React from "react";
 import { ChatPane } from "chains/editor/ChatPane";
 import { ChainState } from "chains/editor/contexts";
 import { useTestChat } from "chains/hooks/useTestChat";
-import { Center, Spinner } from "@chakra-ui/react";
+import { Center, Spinner, Text } from "@chakra-ui/react";
 
 export const TestChatPane = () => {
   const { chain } = React.useContext(ChainState);
   const { chat } = useTestChat(chain?.id);
+
+  if (!chain) {
+    return (
+      <Center height={"100%"}>
+        <Text color={"gray.500"} fontSize={"sm"}>
+          Please save the chain before testing the chat.
+        </Text>
+      </Center>
+    );
+  }
 
   if (!chat)
     return (
