5c5dffe659737d807acbd6443f7528c6d654ba67
Peter Krenesky <kreneskyp@gmail.com>
2023-09-15 21:46:09 -0700

    Update icon for new agents

--- frontend/agents/NewAgentButton.js
+++ frontend/agents/NewAgentButton.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon, IconButton } from "@chakra-ui/react";
+import { IconButton } from "@chakra-ui/react";
 import { useColorMode } from "@chakra-ui/color-mode";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faDiagramProject } from "@fortawesome/free-solid-svg-icons";
 
 export const NewAgentButton = () => {
   const { colorMode } = useColorMode();
@@ -14,8 +14,8 @@ export const NewAgentButton = () => {
         width="100%"
         border="1px solid"
         borderColor={colorMode === "light" ? "gray.300" : "whiteAlpha.50"}
-        icon={<FontAwesomeIcon icon={faUserPlus} />}
-        title={"New Agent"}
+        icon={<FontAwesomeIcon icon={faDiagramProject} />}
+        title={"New Agent or Chain"}
       >
         New Agent
       </IconButton>
