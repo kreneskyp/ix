3f6162ae17963b6d33a914818278ed504b0b707d
Peter Krenesky <kreneskyp@gmail.com>
2023-10-01 19:02:06 -0700

    Fixing chroma field imports

--- ix/chains/fixture_src/vectorstores.py
+++ ix/chains/fixture_src/vectorstores.py
@@ -87,11 +87,16 @@ CHROMA = {
     "description": "Chroma vector database",
     "connectors": VECTORSTORE_CONNECTORS,
     "fields": NodeTypeField.get_fields(
-        Chroma,
+        Chroma.__init__,
         include=[
             "collection_name",
             "persist_directory",
         ],
+        field_options={
+            "persist_directory": {
+                "default": "./chroma",
+            }
+        },
     )
     + NodeTypeField.get_fields(
         chromadb.config.Settings,
