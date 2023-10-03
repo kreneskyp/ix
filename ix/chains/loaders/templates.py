77a5021a6732dcc22eb62ceca88f3bd367cd0c7c
Peter Krenesky <kreneskyp@gmail.com>
2023-10-02 20:27:26 -0700

    Completing NodeTemplate implementation: - Add get_config_variables to get a list of variables in a config dict - Debugging format_config for formatting config dicts - Adding NodeTemplate model, including helper methods for variables, variable schema, and formatting - Adding Connector.template property to specify connectors are templates - load_node now lazy loads templates into NodeTemplate instances - Tests!

---
+++ ix/chains/loaders/templates.py
@@ -0,0 +1,69 @@
+from typing import TypeVar, Generic, Dict, Any, Set, Type
+
+from asgiref.sync import sync_to_async, async_to_sync
+from pydantic import BaseModel, create_model
+
+from ix.chains.loaders.context import IxContext
+from ix.chains.models import ChainNode
+from ix.utils.config import get_config_variables
+
+T = TypeVar("T")
+
+
+class NodeTemplate(Generic[T]):
+    """
+    A template for a node in a chain. This is used to create a node from a graph whose
+    nodes contain $variables.
+
+    Templates containing $variables are used as placeholders to delay instantiation
+    until request time. This allows for dynamic configuration of nodes from chain
+    inputs.
+
+    The most common example is a document loader. Document loaders are used to load
+    documents into a vectorstore. Document loaders generally require a path or URL
+    to the source document. Using a template, document loaders can be reused
+    for different source documents.
+    """
+
+    def __init__(self, node: ChainNode, context: IxContext):
+        self.node = node
+        self.context = context
+
+    def format(self, input: Dict[str, Any]) -> T:
+        from ix.chains.loaders.core import load_node
+
+        return load_node(self.node, self.context, variables=input)
+
+    async def aformat(self, input: Dict[str, Any]) -> T:
+        from ix.chains.loaders.core import load_node
+
+        return await sync_to_async(load_node)(self.node, self.context, variables=input)
+
+    async def get_variables(self, node: ChainNode = None) -> Set[str]:
+        """
+        Helper recursive function to extract config variables.
+        """
+        node = node if node else self.node
+        variables = get_config_variables(node.config if node.config else {})
+
+        # Recursively traverse for all connected nodes
+        connected_edges = node.incoming_edges.filter(relation="PROP").order_by("key")
+        async for edge in connected_edges.aiterator():
+            connected_node = await ChainNode.objects.aget(pk=edge.source_id)
+            variables.update(await self.get_variables(connected_node))
+
+        return variables
+
+    def get_args_schema(self) -> Type[BaseModel]:
+        """
+        Dynamically create a Pydantic model class with fields for each variable in the template.
+        """
+        # Use dictionary comprehension to define fields with type annotations
+        field_definitions = {
+            field: (str, ...) for field in async_to_sync(self.get_variables)()
+        }
+
+        # Create and return the new Pydantic model class using the 'type' function
+        DynamicArgsSchema = create_model("DynamicArgsSchema", **field_definitions)
+
+        return DynamicArgsSchema
