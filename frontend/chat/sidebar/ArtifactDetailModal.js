d3a4413ddda91b5a0918335d5e0c7051e3709b25
Peter Krenesky <kreneskyp@gmail.com>
2023-10-18 21:18:59 -0700

    Artifact modal now has a download link

--- frontend/chat/sidebar/ArtifactDetailModal.js
+++ frontend/chat/sidebar/ArtifactDetailModal.js
@@ -8,11 +8,18 @@ import {
   ModalOverlay,
   Tag,
   TagLabel,
+  Text,
+  Tooltip,
 } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import ArtifactDetail from "chat/sidebar/ArtifactDetail";
 
 export const ArtifactDetailModal = ({ artifact, isOpen, onClose }) => {
   const [hasOpened, setHasOpened] = useState(false);
+  const { colorMode } = useColorMode;
+  const color = colorMode === "light" ? "gray.800" : "gray.400";
 
   useEffect(() => {
     if (!hasOpened) {
@@ -29,6 +36,22 @@ export const ArtifactDetailModal = ({ artifact, isOpen, onClose }) => {
           <Tag size="md" colorScheme="blue" ml={3} mt={1}>
             <TagLabel>{artifact.artifactType}</TagLabel>
           </Tag>
+          <Tooltip label="Download file">
+            <Text as="span" color={color}>
+              <FontAwesomeIcon
+                icon={faDownload}
+                style={{ marginLeft: '10px', cursor: "pointer" }}
+                onClick={() => {
+                  const url = `/api/artifacts/${artifact.id}/download`;
+                  const link = document.createElement('a');
+                  link.href = url;
+                  link.setAttribute('download', 'file');
+                  document.body.appendChild(link);
+                  link.click();
+                }}
+            />
+            </Text>
+            </Tooltip>
         </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
