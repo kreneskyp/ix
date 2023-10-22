174ae85954d72a64ca4aeb41da53c79348acbefb
Peter Krenesky <kreneskyp@gmail.com>
2023-10-22 11:15:24 -0700

    JSONForm is now replacing TypeAutoFields, rending with JSONSchema rather than IX's field types

--- frontend/chains/flow/TypeAutoFields.js
+++ frontend/chains/flow/TypeAutoFields.js
@@ -1,32 +1,36 @@
 import React, { useCallback } from "react";
 import {
+  Box,
   Checkbox,
   Flex,
+  FormControl,
   FormLabel,
   HStack,
   Input,
   Select,
   Textarea,
+  Tooltip,
   VStack,
 } from "@chakra-ui/react";
 import SliderInput from "components/SliderInput";
 import { useEditorColorMode } from "chains/editor/useColorMode";
 import { DictForm } from "components/DictForm";
+import { ListForm } from "components/ListForm";
 
-const getLabel = (field) => field.label || labelify(field.name || "");
+const getLabel = (name) => name || labelify(name || "");
 
-const AutoFieldCheckbox = ({ field, value, onChange }) => {
+const AutoFieldCheckbox = ({ name, field, value, onChange }) => {
   const colorMode = useEditorColorMode();
   const handleChange = useCallback(
     (event) => {
-      onChange(field.name, event.target.checked);
+      onChange(name, event.target.checked);
     },
     [field, onChange]
   );
 
   return (
     <HStack width="100%">
-      <FormLabel justify="start">{getLabel(field)}</FormLabel>
+      <FormLabel justify="start">{getLabel(name)}</FormLabel>
       <Checkbox
         isChecked={value}
         onChange={handleChange}
@@ -36,20 +40,24 @@ const AutoFieldCheckbox = ({ field, value, onChange }) => {
   );
 };
 
-const AutoFieldInput = ({ field, value, onChange }) => {
+const AutoFieldInput = ({ name, field, isRequired, value, onChange }) => {
   const colorMode = useEditorColorMode();
   const handleChange = useCallback(
     (event) => {
-      onChange(field.name, event.target.value);
+      onChange(name, event.target.value);
     },
     [field, onChange]
   );
 
   return (
     <Flex justifyContent="space-between" wrap="wrap" width="100%">
+
       <FormLabel justify="start" whiteSpace="nowrap">
-        {getLabel(field)}
+          <Tooltip label={field.description}>
+            <Box p={0} m={0}>{getLabel(name)}</Box>
+          </Tooltip>
       </FormLabel>
+
       <Input
         value={value || ""}
         onChange={handleChange}
@@ -62,11 +70,11 @@ const AutoFieldInput = ({ field, value, onChange }) => {
   );
 };
 
-const AutoFieldSecret = ({ field, value, onChange }) => {
+const AutoFieldSecret = ({ name, field, isRequired, value, onChange }) => {
   const colorMode = useEditorColorMode();
   const handleChange = useCallback(
     (event) => {
-      onChange(field.name, event.target.value);
+      onChange(name, event.target.value);
     },
     [field, onChange]
   );
@@ -74,7 +82,7 @@ const AutoFieldSecret = ({ field, value, onChange }) => {
   return (
     <Flex justifyContent="space-between" wrap="wrap" width="100%">
       <FormLabel justify="start" whiteSpace="nowrap">
-        {getLabel(field)}
+        {getLabel(name)}
       </FormLabel>
       <Input
         value={value || ""}
@@ -89,11 +97,11 @@ const AutoFieldSecret = ({ field, value, onChange }) => {
   );
 };
 
-const AutoFieldTextArea = ({ field, value, onChange }) => {
+const AutoFieldTextArea = ({ name, field, isRequired, value, onChange }) => {
   const colorMode = useEditorColorMode();
   const handleChange = useCallback(
     (event) => {
-      onChange(field.name, event.target.value);
+      onChange(name, event.target.value);
     },
     [field, onChange]
   );
@@ -101,7 +109,7 @@ const AutoFieldTextArea = ({ field, value, onChange }) => {
   return (
     <Flex justifyContent="space-between" wrap="wrap" width="100%">
       <FormLabel justify="start" whiteSpace="nowrap">
-        {getLabel(field)}
+        {getLabel(name)}
       </FormLabel>
       <Textarea
         fontSize="sm"
@@ -116,37 +124,37 @@ const AutoFieldTextArea = ({ field, value, onChange }) => {
   );
 };
 
-const AutoFieldSlider = ({ field, value, onChange }) => {
+const AutoFieldSlider = ({ name, field, value, onChange }) => {
   const handleChange = useCallback(
     (newValue) => {
-      onChange(field.name, newValue);
+      onChange(name, newValue);
     },
     [field, onChange]
   );
 
   return (
     <SliderInput
-      label={getLabel(field)}
-      field={field.name}
+      label={getLabel(name)}
+      field={name}
       value={value}
       onChange={handleChange}
-      min={field.min}
-      max={field.max}
-      step={field.step}
+      min={field.minimum}
+      max={field.maximum}
+      step={field.multipleOf}
     />
   );
 };
 
-const AutoFieldSelect = ({ field, value, onChange }) => {
+const AutoFieldSelect = ({ name, field, isRequired, value, onChange }) => {
   const colorMode = useEditorColorMode();
   const handleChange = useCallback((event) => {
-    onChange(field.name, event.target.value);
+    onChange(name, event.target.value);
   });
 
   return (
     <Flex width="100%" justifyContent="space-between">
       <FormLabel size="sm" justify="start">
-        {getLabel(field)}
+        {getLabel(name)}
       </FormLabel>
       <Select
         onChange={handleChange}
@@ -154,9 +162,9 @@ const AutoFieldSelect = ({ field, value, onChange }) => {
         value={value}
         {...colorMode.input}
       >
-        {field.choices?.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
+        {field.enum?.map((value) => (
+          <option key={value} value={value}>
+            {value}
           </option>
         ))}
       </Select>
@@ -164,12 +172,12 @@ const AutoFieldSelect = ({ field, value, onChange }) => {
   );
 };
 
-const AutoFieldDict = ({ field, value, onChange }) => {
+const AutoFieldDict = ({ name, field, isRequired, value, onChange }) => {
   const handleChange = useCallback(
     (newValue) => {
       // Check if newValue is null or undefined, if so, set it to an empty object
       newValue = newValue || {};
-      onChange(field.name, newValue);
+      onChange(name, newValue);
     },
     [field, onChange]
   );
@@ -178,9 +186,28 @@ const AutoFieldDict = ({ field, value, onChange }) => {
   value = value || {};
 
   return (
-    <DictForm dict={value} onChange={handleChange} label={getLabel(field)} />
+    <DictForm dict={value} onChange={handleChange} label={getLabel(name)} isRequired />
   );
 };
+
+const AutoFieldList = ({ name, field, isRequired, value, onChange }) => {
+  const handleChange = useCallback(
+    (newValue) => {
+      // Check if newValue is null or undefined, if so, set it to an empty array
+      newValue = newValue || [];
+      onChange(name, newValue);
+    },
+    [field, onChange]
+  );
+
+  // Check if value is null or undefined, if so, set it to an empty array
+  value = value || [];
+
+  return (
+    <ListForm list={value} onChange={handleChange} label={getLabel(name)} isRequired />
+  );
+};
+
 
 // explicit input types
 const INPUTS = {
@@ -191,6 +218,7 @@ const INPUTS = {
   secret: AutoFieldSecret,
   textarea: AutoFieldTextArea,
   dict: AutoFieldDict,
+  list: AutoFieldList,
 };
 
 // type specific default inputs
@@ -198,6 +226,9 @@ const TYPE_INPUTS = {
   boolean: "checkbox",
   bool: "checkbox",
   dict: "dict",
+  list: "list",
+  array: "list",
+  object: "dict",
 };
 
 const DEFAULT_COMPONENT = AutoFieldInput;
@@ -212,37 +243,59 @@ const FIELD_TYPE_DEFAULTS = {
   string: "",
   list: [],
   dict: {},
+  object: {},
 };
 
+// This is used elsewhere in graph editor
 export const getDefaults = (nodeType) => {
   const defaults = {};
   nodeType.fields?.forEach(
     (field) =>
-      (defaults[field.name] = field.default || FIELD_TYPE_DEFAULTS[field.type])
+      (defaults[name] = field.default || FIELD_TYPE_DEFAULTS[field.type])
   );
   return defaults;
 };
-export const TypeAutoField = ({ field, config, onChange }) => {
+
+export const TypeAutoField = ({ name, field, isRequired, config, onChange }) => {
   // Select component based on explicit input type, field type, or default in that order.
   const FieldComponent =
     INPUTS[field.input_type || TYPE_INPUTS[field.type]] || DEFAULT_COMPONENT;
   const value =
-    config && field.name in config ? config[field.name] : field.default;
+    config && name in config ? config[name] : field.default;
 
-  return <FieldComponent field={field} value={value} onChange={onChange} />;
+  return (
+    <FormControl>
+      <HStack>
+        <FieldComponent name={name} field={field} isRequired={isRequired} value={value} onChange={onChange} />
+      </HStack>
+    </FormControl>
+  );
 };
+
+
+export const JSONSchemaForm = ({ schema, data, onChange }) => {
+  return (
+    <VStack spacing={3} width="100%">
+      {Object.keys(schema?.properties).map((fieldName) => {
+        const field = schema.properties[fieldName];
+        return (
+          <TypeAutoField
+            key={fieldName}
+            field={field}
+            name={fieldName}
+            isRequired={schema.required.includes(fieldName)}
+            config={data}
+            onChange={onChange}
+          />
+        );
+      })}
+    </VStack>
+  );
+};
+
 
 export const TypeAutoFields = ({ type, config, onChange }) => {
   return (
-    <VStack spacing={1} width="100%">
-      {type.fields?.map((field) => (
-        <TypeAutoField
-          key={field.name}
-          field={field}
-          config={config}
-          onChange={onChange}
-        />
-      ))}
-    </VStack>
-  );
+    <JSONSchemaForm schema={type?.config_schema} data={config} onChange={onChange} />
+    );
 };
