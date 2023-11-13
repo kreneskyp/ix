d725e2c31a01499ea2c071ed9715d0ba0ecca19b
Peter Krenesky <kreneskyp@gmail.com>
2023-11-13 08:47:37 -0800

    Implmenting load and init methods for flow created by LINK edges with sequences, branch, and maps.

--- ix/chains/loaders/core.py
+++ ix/chains/loaders/core.py
@@ -2,22 +2,24 @@ import itertools
 import logging
 import time
 from collections import defaultdict
-from typing import Callable, Any, List
+from dataclasses import dataclass
+from typing import Callable, Any, List, Tuple, Dict
 
+from asgiref.sync import sync_to_async
 from langchain.schema.runnable import (
-    RunnableSequence,
-    RunnablePassthrough,
     RunnableSerializable,
+    RunnableSequence,
+    RunnableParallel,
+    Runnable,
 )
-from langchain.schema.runnable.utils import Input, Output
 
-from ix.chains.components.lcel import init_runnable_sequence
+from ix.chains.components.lcel import init_sequence, init_branch
 from ix.chains.loaders.context import IxContext
 from langchain.chains.base import Chain as LangchainChain
 
 from ix.chains.loaders.prompts import load_prompt
 from ix.chains.loaders.templates import NodeTemplate
-from ix.chains.models import NodeType, ChainNode, ChainEdge
+from ix.chains.models import NodeType, ChainNode, ChainEdge, Chain
 from ix.secrets.models import Secret
 from ix.utils.config import format_config
 from ix.utils.importlib import import_class
@@ -26,6 +28,16 @@ import_node_class = import_class
 
 
 logger = logging.getLogger(__name__)
+
+
+RUNNABLE_TYPES = {
+    "agent",
+    "chain",
+    "llm",
+    "prompt",
+    "retriever",
+    "tool",
+}
 
 
 def get_node_loader(name: str) -> Callable:
@@ -251,35 +263,305 @@ def load_node(
     return instance
 
 
-def load_sequence(
-    first_node: ChainNode,
-    first_instance: LangchainChain,
+def load_collection(
+    connector: dict,
+    edge_group: List[ChainEdge],
     context: IxContext,
-) -> List[LangchainChain]:
+    variables: Dict[str, Any] = None,
+) -> RunnableSerializable | List[Tuple[str, RunnableSerializable]]:
+    """Load connector as a collection (list, map, map-tuples, etc). These properties
+    are special in that the graph is traversed and contained within flow control
+    `Runnable` such as `RunnableSequence`, `RunnableMa`p, etc. This bridges the gap
+    between how a graph is structured as nodes  &edges in the database/UX and how it
+    is represented with LangChain Expression Language components.
     """
-    Load a sequence of nodes.
-    """
-    sequential_nodes = [first_instance]
+    connector_is_template = connector.get("template", False)
+    collection_type = connector.get("collection", None)
 
-    # handle linked nodes
-    # for now only a single outgoing link is supported
-    outgoing_link = None
-    try:
-        outgoing_link = first_node.outgoing_edges.select_related("target").get(
-            relation="LINK"
-        )
-    except ChainEdge.DoesNotExist:
-        pass
+    if collection_type in {"flow"}:
+        if collection_type == "flow":
+            # load property as a Runnable flow
+            nodes = [edge.source for edge in edge_group]
+            return init_flow_node(nodes, context=context, variables=variables)
+    elif collection_type == "map":
+        return {
+            edge.map_key: load_node(
+                edge.source,
+                context,
+                variables=variables,
+                as_template=connector_is_template,
+            )
+            for edge in edge_group
+        }
+    elif collection_type == "map_tuples":
+        return [
+            (
+                edge.map_key,
+                load_node(
+                    edge.source,
+                    context,
+                    variables=variables,
+                    as_template=connector_is_template,
+                ),
+            )
+            for edge in edge_group
+        ]
+    else:
+        raise ValueError(f"Unknown collection type: {collection_type}")
+
+
+@dataclass
+class MapPlaceholder:
+    node: ChainNode
+    map: Dict[str, "FlowPlaceholder"]
+
+
+@dataclass
+class BranchPlaceholder:
+    node: ChainNode
+    branches: List[Tuple[str, "FlowPlaceholder"]]
+    default: "FlowPlaceholder"
+
+
+FlowPlaceholder = (
+    ChainNode | List["FlowPlaceholder"] | MapPlaceholder | BranchPlaceholder
+)
+
+
+def init_chain_flow(
+    chain: Chain, context: IxContext, variables: Dict[str, Any] = None
+) -> Runnable:
+    """
+    Initialize a flow from a chain.
+    """
+    flow_root = load_chain_flow(chain=chain)
+    logger.debug(f"init_chain_flow chain={chain.id} flow_root={flow_root}")
+    return init_flow_node(flow_root, context=context, variables=variables)
+
+
+async def ainit_chain_flow(
+    chain: Chain, context: IxContext, variables: Dict[str, Any] = None
+):
+    return await sync_to_async(init_chain_flow)(chain, context, variables)
+
+
+def init_flow(
+    nodes: List[ChainNode], context: IxContext, variables: Dict[str, Any] = None
+) -> Runnable:
+    flow_root = load_flow_node(nodes)
+    return init_flow_node(flow_root, context=context, variables=variables)
+
+
+async def ainit_flow(
+    nodes: List[ChainNode], context: IxContext, variables: Dict[str, Any] = None
+) -> Runnable:
+    return await sync_to_async(init_flow)(nodes, context, variables)
+
+
+def load_chain_flow(chain: Chain) -> FlowPlaceholder:
+    roots = chain.nodes.filter(root=True)
+    logger.debug(f"Loading chain flow with roots: {roots}")
+    return load_flow_node(roots)
+
+
+async def aload_chain_flow(chain: Chain) -> FlowPlaceholder:
+    return await sync_to_async(load_chain_flow)(chain)
+
+
+def load_flow_node(nodes: List[ChainNode]) -> FlowPlaceholder:
+    """Loads a node or group of node connected to a map"""
+    if len(nodes) == 0:
+        raise ValueError("No root nodes found")
+
+    seen = {}
+    print("load_flow_node: ", nodes)
+    if len(nodes) == 1:
+        return load_flow_sequence(nodes[0], seen)
+    return load_flow_map(nodes, seen)
+
+
+async def aload_flow_node(chain: Chain, context=None, variables=None) -> None:
+    return await sync_to_async(load_flow_node)(chain)
+
+
+def load_flow_map(
+    nodes: List[ChainNode],
+    seen: Dict[str, FlowPlaceholder],
+) -> FlowPlaceholder:
+    logger.debug(">>>LOADING MAP")
+    new_nodes = []
+    for node in nodes:
+        logger.debug(f">>>>>>> {node}")
+        new_nodes = load_flow_sequence(node, seen=seen)
+
+    # Nested maps collapse into the last node where all branches of the edges
+    # merge back in. It doesn't matter which return value is used here since
+    # they are all the same.
+    return new_nodes
+
+
+def load_flow_branch(
+    node: ChainNode,
+    seen: Dict[str, FlowPlaceholder],
+) -> BranchPlaceholder:
+    # gather branches
+    outgoing_links = (
+        node.outgoing_edges.select_related("target")
+        .filter(relation="LINK")
+        .order_by("source_key")
+    )
+    branches = {}
+    for key, group in itertools.groupby(outgoing_links, lambda edge: edge.source_key):
+        group_as_list = list(group)
+        if len(group_as_list) > 1:
+            targets = [edge.target for edge in group_as_list]
+            nodes = load_flow_map(targets, seen=seen)
+        else:
+            nodes = load_flow_sequence(group_as_list[0].target, seen=seen)
+        branches[key] = nodes
+
+    # build sorted list from branch node's config
+    branch_keys = node.config.get("branches", [])
+    branch_uuids = node.config.get("branches_hash", [])
+    branch_tuples = [
+        (key, branches[branch_uuid])
+        for key, branch_uuid in zip(branch_keys, branch_uuids)
+    ]
+
+    if "default" not in branches:
+        raise ValueError("Branch node must have a default branch")
+
+    return BranchPlaceholder(
+        node=node, default=branches["default"], branches=branch_tuples
+    )
+
+
+BRANCH_CLASS_PATH = "ix.chains.components.lcel.init_branch"
+MAP_CLASS_PATH = "ix.chains.components.lcel.init_parallel"
+
+
+def load_flow_sequence(
+    start: ChainNode,
+    seen: Dict[str, FlowPlaceholder],
+) -> Runnable | RunnableSequence:
+    sequential_nodes = []
 
     # traverse the sequence
-    while outgoing_link:
-        next_instance = outgoing_link.target.load(context, root=False)
-        sequential_nodes.append(next_instance)
-        try:
-            outgoing_link = outgoing_link.target.outgoing_edges.select_related(
-                "target"
-            ).get(relation="LINK")
-        except ChainEdge.DoesNotExist:
-            outgoing_link = None
+    current = start
+    infinite_loop_safety_count = 0
+    while infinite_loop_safety_count < 1000:
+        infinite_loop_safety_count += 1
 
-    return sequential_nodes
+        outgoing_links = list(
+            current.outgoing_edges.select_related("target").filter(relation="LINK")
+        )
+
+        if len(outgoing_links) > 1 and current.class_path != BRANCH_CLASS_PATH:
+            # multiple links: start of implicitly defined map
+            targets = [link.target for link in outgoing_links]
+            map_nodes = load_flow_map(targets, seen=seen)
+            sequential_nodes.append(current)
+            sequential_nodes.extend(
+                map_nodes if isinstance(map_nodes, list) else [map_nodes]
+            )
+
+            # Node for next iteration is the last node in the sequence that is returned
+            current = sequential_nodes[-1]
+            if isinstance(current, MapPlaceholder):
+                current = current.node
+
+        else:
+            # single outgoing link
+            if outgoing_links and outgoing_links[0].target.class_path == MAP_CLASS_PATH:
+                # Since MAP node comes after the branches feeding into it,
+                # look at outgoing LINKs to determine if the current sequence
+                # should be aggregated forward into it.
+                target = outgoing_links[0].target
+                node_map = seen.setdefault(
+                    target.id, MapPlaceholder(node=target, map={})
+                )
+                if not sequential_nodes:
+                    map_sequence = current
+                elif current.class_path == MAP_CLASS_PATH:
+                    map_sequence = sequential_nodes
+                else:
+                    map_sequence = sequential_nodes + [current]
+                if isinstance(map_sequence, list) and len(map_sequence) == 1:
+                    map_sequence = map_sequence[0]
+
+                # resolve the map connector hash into the key
+                target_key = outgoing_links[0].target_key
+                step_index = target.config["steps_hash"].index(target_key)
+                map_key = target.config["steps"][step_index]
+                node_map.map[map_key] = map_sequence
+
+                # add to sequence here since it's a placeholder and current
+                # must be a node
+                sequential_nodes = [node_map]
+
+                current = target
+                continue
+
+            elif current.class_path == BRANCH_CLASS_PATH:
+                # start of explicitly defined branch
+                branch_placeholder = load_flow_branch(current, seen=seen)
+                sequential_nodes.append(branch_placeholder)
+
+                # branches must be fully explored so there is nothing remaining to traverse
+                break
+            else:
+                # Add nodes that were not already added in the forward traversal for
+                # a map node. This includes map node and last node in the map sequence
+                if current.class_path != MAP_CLASS_PATH and (
+                    not sequential_nodes or sequential_nodes[-1] != current
+                ):
+                    if current.id in seen:
+                        sequential_nodes.append(seen[current.id])
+                    else:
+                        sequential_nodes.append(current)
+                        seen[current.id] = current
+
+            if len(outgoing_links) == 0:
+                break
+
+            # there should only be one outgoing link here.
+            current = outgoing_links[0].target
+
+    if len(sequential_nodes) == 1:
+        return sequential_nodes[0]
+    else:
+        return sequential_nodes
+
+
+def init_flow_node(
+    root: FlowPlaceholder, context: IxContext, variables: Dict[str, Any] = None
+) -> Runnable:
+    """Initial a flow node
+
+    Assumes a collection of ChainNodes and placeholders constructed by load_flow.
+    """
+
+    if isinstance(root, ChainNode):
+        return load_node(root, context=context, variables=variables)
+    elif isinstance(root, BranchPlaceholder):
+        return init_branch(
+            default=init_flow_node(root.default, context=context, variables=variables),
+            branches=[
+                (key, init_flow_node(branch, context=context, variables=variables))
+                for key, branch in root.branches
+            ],
+        )
+    elif isinstance(root, MapPlaceholder):
+        return RunnableParallel(
+            **{
+                key: init_flow_node(node, context=context, variables=variables)
+                for key, node in root.map.items()
+            }
+        )
+    elif isinstance(root, list):
+        nodes = [
+            init_flow_node(node, context=context, variables=variables) for node in root
+        ]
+        return init_sequence(steps=nodes)
+    else:
+        raise Exception("Invalid flow type: " + str(type(root)))
