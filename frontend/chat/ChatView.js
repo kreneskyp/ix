17160445767b064ee5a5bc29b32aecaea79a0c68
Peter Krenesky <kreneskyp@gmail.com>
2023-11-14 17:59:07 -0800

    Adding ChatInputProvider to ChatView

--- frontend/chat/ChatView.js
+++ frontend/chat/ChatView.js
@@ -31,6 +31,7 @@ import { useLinkedScroll } from "hooks/useLinkedScroll";
 import { ChatGraph, ChatAgents } from "chat/contexts";
 import { AgentCardListButton } from "agents/AgentCardListButton";
 import { ChatAgentCard } from "chat/agents/ChatAgentCard";
+import { ChatInputProvider } from "chat/input/ChatInputProvider";
 
 export const ChatLeftPaneShim = ({ graph }) => {
   const chatAgentsAPI = usePaginatedAPI(`/api/agents/`, {
@@ -93,6 +94,10 @@ export const ChatRightSidebar = ({ graph, disclosure, drawerRef }) => {
   );
 };
 
+export const ChatViewProvider = ({ children }) => {
+  return <ChatInputProvider>{children}</ChatInputProvider>;
+};
+
 export const ChatView = () => {
   const { id } = useParams();
   const { response, call: loadGraph, isLoading } = useChatGraph(id);
@@ -109,43 +114,49 @@ export const ChatView = () => {
   }, [id]);
 
   return (
-    <Layout>
-      <LayoutLeftPane>
-        {isLoading || !graph ? <Spinner /> : <ChatLeftPaneShim graph={graph} />}
-      </LayoutLeftPane>
-      <LayoutContent>
-        {isLoading || !graph ? (
-          <Spinner />
-        ) : (
-          <HStack justifyContent={"start"}>
-            <Box
-              width="100%"
-              height="100vh"
-              display="flex"
-              flexDirection="column"
-            >
-              <ChatInterface
+    <ChatViewProvider>
+      <Layout>
+        <LayoutLeftPane>
+          {isLoading || !graph ? (
+            <Spinner />
+          ) : (
+            <ChatLeftPaneShim graph={graph} />
+          )}
+        </LayoutLeftPane>
+        <LayoutContent>
+          {isLoading || !graph ? (
+            <Spinner />
+          ) : (
+            <HStack justifyContent={"start"}>
+              <Box
+                width="100%"
+                height="100vh"
+                display="flex"
+                flexDirection="column"
+              >
+                <ChatInterface
+                  graph={graph}
+                  scrollboxProps={{ ref: scrollBoxRef }}
+                />
+              </Box>
+              <Box position="absolute" top={0} right={0} mt={4} mr={4}>
+                <IconButton
+                  ml="auto"
+                  icon={<FontAwesomeIcon icon={faRightLeft} />}
+                  onClick={rightSidebarDisclosure.onOpen}
+                  aria-label="Open Sidebar"
+                  title={"Open Sidebar"}
+                />
+              </Box>
+              <ChatRightSidebar
                 graph={graph}
-                scrollboxProps={{ ref: scrollBoxRef }}
+                disclosure={rightSidebarDisclosure}
+                drawerRef={drawerRef}
               />
-            </Box>
-            <Box position="absolute" top={0} right={0} mt={4} mr={4}>
-              <IconButton
-                ml="auto"
-                icon={<FontAwesomeIcon icon={faRightLeft} />}
-                onClick={rightSidebarDisclosure.onOpen}
-                aria-label="Open Sidebar"
-                title={"Open Sidebar"}
-              />
-            </Box>
-            <ChatRightSidebar
-              graph={graph}
-              disclosure={rightSidebarDisclosure}
-              drawerRef={drawerRef}
-            />
-          </HStack>
-        )}
-      </LayoutContent>
-    </Layout>
+            </HStack>
+          )}
+        </LayoutContent>
+      </Layout>
+    </ChatViewProvider>
   );
 };
