db5888336a250d0dad9f8f40e876d864a46deb75
Peter Krenesky <kreneskyp@gmail.com>
2023-11-15 08:31:03 -0800

    Missing import

--- ix/chains/models.py
+++ ix/chains/models.py
@@ -6,6 +6,7 @@ from typing import Any, Dict
 
 from django.db import models
 from langchain.chains.base import Chain as LangChain
+from langchain.schema.runnable import Runnable
 
 from ix.ix_users.models import OwnedModel
 from ix.pg_vector.tests.models import PGVectorMixin
