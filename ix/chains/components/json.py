e754be483f9a6e85e678108f3a634d42cc671f9f
Peter Krenesky <kreneskyp@gmail.com>
2023-11-21 11:50:21 -0800

    JSONPath improvements: - can now return a list of matching results - now uses to_json_serializable to handle dataclasses and pydantic models better

--- ix/chains/components/json.py
+++ ix/chains/components/json.py
@@ -4,6 +4,8 @@ from jsonpath_ng import parse as jsonpath_parse
 from langchain.schema.runnable import RunnableSerializable, RunnableConfig
 from langchain.schema.runnable.utils import Input, Output
 
+from ix.utils.json import to_json_serializable
+
 
 class JSONPath(RunnableSerializable[Input, Output]):
     """Parse a value from inputs using a JSONPath"""
@@ -11,16 +13,22 @@ class JSONPath(RunnableSerializable[Input, Output]):
     path: str
     """JSON Path to load."""
 
+    return_list: bool = False
+    """If True, return results as a list."""
+
     def invoke(
         self,
         input: Dict[str, Any],
         config: Optional[RunnableConfig] = None,
         **kwargs: Any,
-    ):
+    ) -> Output:
         jsonpath_expr = jsonpath_parse(self.path)
-        json_matches = jsonpath_expr.find(input)
-        if len(json_matches) == 0:
-            raise ValueError(f"could not find input at {self.path} searched: {input}")
+        formatted_input = to_json_serializable(input)
+        json_matches = jsonpath_expr.find(formatted_input)
 
-        value = json_matches[0].value
-        return value
+        if len(json_matches) > 1 or self.return_list:
+            return [match.value for match in json_matches]
+        elif len(json_matches) == 0:
+            raise ValueError(f"could not find input at {self.path} searched: {input}")
+        else:
+            return json_matches[0].value
