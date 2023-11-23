047d985c222a11c75984c0ce681a8b0fc707ce01
Peter Krenesky <kreneskyp@gmail.com>
2023-11-23 10:53:03 -0800

    JSONPath: now has ainvoke method

--- ix/chains/components/json.py
+++ ix/chains/components/json.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
 from jsonpath_ng import parse as jsonpath_parse
 
 from langchain.schema.runnable import RunnableSerializable, RunnableConfig
@@ -7,7 +7,7 @@ from langchain.schema.runnable.utils import Input, Output
 from ix.utils.json import to_json_serializable
 
 
-class JSONPath(RunnableSerializable[Input, Output]):
+class JSONPath(RunnableSerializable[Input, Output | List[Output]]):
     """Parse a value from inputs using a JSONPath"""
 
     path: str
@@ -16,7 +16,29 @@ class JSONPath(RunnableSerializable[Input, Output]):
     return_list: bool = False
     """If True, return results as a list."""
 
+    @classmethod
+    def is_lc_serializable(cls) -> bool:
+        """Is this class serializable?"""
+        return True
+
     def invoke(
+        self,
+        input: Dict[str, Any],
+        config: Optional[RunnableConfig] = None,
+        **kwargs: Any,
+    ) -> Output | List[Output]:
+        jsonpath_expr = jsonpath_parse(self.path)
+        formatted_input = to_json_serializable(input)
+        json_matches = jsonpath_expr.find(formatted_input)
+
+        if len(json_matches) > 1 or self.return_list:
+            return [match.value for match in json_matches]
+        elif len(json_matches) == 0:
+            raise ValueError(f"could not find input at {self.path} searched: {input}")
+        else:
+            return json_matches[0].value
+
+    async def ainvoke(
         self,
         input: Dict[str, Any],
         config: Optional[RunnableConfig] = None,
