74e6654a1c9dca80a4e8d339cb3f3ece4ed567f2
Peter Krenesky <kreneskyp@gmail.com>
2023-12-08 18:01:28 -0800

    Adding tests for run flow based coder

--- ix/chains/tests/test_coder.py
+++ ix/chains/tests/test_coder.py
@@ -1,29 +1,33 @@
 import pytest
-from django.core.management import call_command
-from langchain.chains import SequentialChain
 
-from ix.chains.artifacts import SaveArtifact
-from ix.chains.llm_chain import LLMChain
-from ix.chains.management.commands.create_coder_v2 import CODER_V2_CHAIN
+from ix.api.artifacts.types import Artifact
 from ix.chains.models import Chain
-from ix.chains.routing import MapSubchain
+from ix.conftest import aload_fixture
 
 
 @pytest.mark.django_db
-class TestCreateCoder:
-    def test_create_coder(self, mock_openai_key, ix_context):
-        call_command("create_coder_v2")
+class TestFlowCoder:
+    async def test_create_flow_coder(self, anode_types, aix_context, aix_handler):
+        """Sanity check that fixture can be loaded and initialized"""
+        await aload_fixture("agent/code2")
+        chain = await Chain.objects.aget(agent__alias="code2")
 
-        model_instance = Chain.objects.get(id=CODER_V2_CHAIN)
-        chain = model_instance.load_chain(ix_context)
+        # init flow
+        await chain.aload_chain(context=aix_context)
 
-        # assert structure of main sequence
-        assert isinstance(chain, SequentialChain)
-        assert isinstance(chain.chains[0], LLMChain)
-        assert isinstance(chain.chains[1], SaveArtifact)
-        assert isinstance(chain.chains[2], MapSubchain)
+    @pytest.mark.openai_api
+    async def test_invoke_flow_coder(self, anode_types, aix_context, aix_handler):
+        """Test running coder flow"""
+        await aload_fixture("agent/code2")
+        chain = await Chain.objects.aget(agent__alias="code2")
 
-        # assert structure of mapsubchain
-        mapsubchain = chain.chains[2]
-        assert isinstance(mapsubchain.chains[0], LLMChain)
-        assert isinstance(mapsubchain.chains[1], SaveArtifact)
+        # init flow
+        runnable = await chain.aload_chain(context=aix_context)
+
+        result = await runnable.ainvoke(
+            input={"user_input": "write a python fizzbuzz"},
+            config={"callbacks": [aix_handler]},
+        )
+        assert isinstance(result, list)
+        assert len(result) >= 1
+        assert isinstance(result[0], Artifact)
