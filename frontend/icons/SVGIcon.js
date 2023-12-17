f84732ef8f788a5f6844eb3d8053f218b80b74d9
Peter Krenesky <kreneskyp@gmail.com>
2023-12-17 10:16:02 -0800

    Adding SVGIcon, renders an svg icon with color & bg props that support chakra styling

---
+++ frontend/icons/SVGIcon.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, useToken } from "@chakra-ui/react";
+
+export const SVGIcon = ({ color, bg, children, svgProps, ...props }) => {
+  const colorToken = useToken("colors", color || "white");
+
+  return (
+    <Box display={"inline-block"} m={0} p={0} {...props}>
+      <svg
+        width="1.2em"
+        height="1.2em"
+        viewBox="0 0 128 128"
+        fill={colorToken}
+        {...(svgProps || {})}
+      >
+        {children}
+      </svg>
+    </Box>
+  );
+};
+
+export default SVGIcon;
