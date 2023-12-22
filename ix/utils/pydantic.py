a83c2111839773c0052e4da1b41b79f75beb77dd
Peter Krenesky <kreneskyp@gmail.com>
2023-12-21 18:55:24 -0800

    implement model_for_signature helper for building pydantic models to validate a method's inputs

--- ix/utils/pydantic.py
+++ ix/utils/pydantic.py
@@ -1,4 +1,5 @@
-from typing import Any, Type
+import inspect
+from typing import Any, Type, Callable, get_type_hints, List
 
 import pydantic
 from pydantic import BaseModel, create_model
@@ -29,3 +30,39 @@ def create_args_model(variables, name="DynamicModel") -> Type[BaseModel]:
     """
     field_definitions = {field: (str, ...) for field in variables}
     return create_model(name, **field_definitions)
+
+
+def fields_from_signature(func: Callable) -> dict[str, tuple[type, Any]]:
+    # Extract the signature of the __init__ method
+    init_signature = inspect.signature(func)
+
+    # Get type hints, including resolving forward references
+    type_hints = get_type_hints(func)
+
+    # Prepare fields for the Pydantic model
+    fields = {
+        param_name: (
+            type_hints.get(param_name, param.annotation),
+            param.default if param.default is not inspect.Parameter.empty else ...,
+        )
+        for param_name, param in init_signature.parameters.items()
+        if param.kind not in {param.VAR_POSITIONAL, param.VAR_KEYWORD}
+        and param_name not in {"self", "cls"}
+    }
+
+    return fields
+
+
+def model_from_signature(name: str, func: Callable | List[Callable]) -> Type[BaseModel]:
+    """Generate a Pydantic model based on the __init__ method of a given class."""
+    if isinstance(func, list):
+        fields = {}
+        for f in func:
+            fields.update(fields_from_signature(f))
+    else:
+        fields = fields_from_signature(func)
+
+    # Create the Pydantic model dynamically with arbitrary types allowed
+    model_config = {"arbitrary_types_allowed": True}
+    dynamic_model = create_model(name, __config__=model_config, **fields)
+    return dynamic_model
