05a0c3c9a9b44cf5946060371b8699ce35b43771
Peter Krenesky <kreneskyp@gmail.com>
2024-01-06 19:02:41 -0800

    MenuItem now have tooltips

--- frontend/agents/NewAgentButton.js
+++ frontend/agents/NewAgentButton.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Tooltip } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiagramProject } from "@fortawesome/free-solid-svg-icons";
 import { MenuItem } from "site/MenuItem";
 
 export const NewAgentButton = () => {
   return (
-    <Tooltip label="Flow Editor" aria-label="Flow Editor">
-      <Link to="/chains">
-        <MenuItem title={"Flow Editor"}>
-          <FontAwesomeIcon icon={faDiagramProject} />
-        </MenuItem>
-      </Link>
-    </Tooltip>
+    <Link to="/chains">
+      <MenuItem title={"Flow Editor"}>
+        <FontAwesomeIcon icon={faDiagramProject} />
+      </MenuItem>
+    </Link>
   );
 };
--- frontend/site/MenuItem.js
+++ frontend/site/MenuItem.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box, HStack, Text, Tooltip } from "@chakra-ui/react";
 
 export const MenuItem = ({ children, wtf, title, ...props }) => {
   return (
-    <HStack
-      overflow="hidden"
-      w={"100%"}
-      align="center"
-      {...props}
-      cursor={"pointer"}
-      _hover={{
-        color: "gray.500",
-      }}
-    >
-      <Box minW="25px" w={"25px"} pl={"2px"}>
-        {children}
-      </Box>
-      <Text fontSize={"sm"} whiteSpace="nowrap">
-        {title}
-      </Text>
-    </HStack>
+    <Tooltip label={title} aria-label={title}>
+      <HStack
+        overflow="hidden"
+        w={"100%"}
+        align="center"
+        {...props}
+        cursor={"pointer"}
+        _hover={{
+          color: "gray.500",
+        }}
+      >
+        <Box minW="25px" w={"25px"} pl={"2px"}>
+          {children}
+        </Box>
+        <Text fontSize={"sm"} whiteSpace="nowrap">
+          {title}
+        </Text>
+      </HStack>
+    </Tooltip>
   );
 };
