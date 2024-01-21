e60c8be52392c5f6f68de5fdb2d8b4179db2fb8a
Peter Krenesky <kreneskyp@gmail.com>
2024-01-21 14:55:22 -0800

    OpenAPI actions are now draggable from the spec viewer.

--- frontend/schemas/openapi/SchemaPathsList.js
+++ frontend/schemas/openapi/SchemaPathsList.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Box, HStack, Badge, Text } from "@chakra-ui/react";
 import { CollapsibleSection } from "chains/flow/CollapsibleSection";
 import { useEditorColorMode } from "chains/editor/useColorMode";
+import { DraggableNode } from "chains/editor/DraggableNode";
+import { labelify } from "json_form/utils";
+import { DRAGGABLE_CONFIG } from "schemas/openapi/OpenAPIDraggable";
 
 export const METHOD_COLORS = {
   get: "green.400",
@@ -14,7 +17,7 @@ export const METHOD_COLORS = {
   trace: "gray.300",
 };
 
-const SchemaPath = ({ path, method, methodInfo }) => {
+const SchemaPath = ({ schema_id, server, path, method, methodInfo }) => {
   const { isLight } = useEditorColorMode();
   const descriptionStyle = isLight
     ? { color: "gray.700" }
@@ -22,14 +25,22 @@ const SchemaPath = ({ path, method, methodInfo }) => {
 
   return (
     <HStack key={path}>
-      <Badge
-        bg={METHOD_COLORS[method]}
-        color={"white"}
-        width={12}
-        alignItems={"center"}
+      <DraggableNode
+        name={`${methodInfo.operationId}`}
+        description={methodInfo.summary}
+        config={{ schema_id, server, path, method }}
+        class_path={DRAGGABLE_CONFIG.class_path}
       >
-        {method}
-      </Badge>
+        <Badge
+          bg={METHOD_COLORS[method]}
+          color={"white"}
+          width={12}
+          alignItems={"center"}
+          zIndex={1}
+        >
+          {method}
+        </Badge>
+      </DraggableNode>
       <Text>{path}</Text>
       <Text fontSize={"xs"} {...descriptionStyle}>
         {methodInfo.summary}
@@ -38,7 +49,7 @@ const SchemaPath = ({ path, method, methodInfo }) => {
   );
 };
 
-export const SchemaPathsList = ({ schema }) => {
+export const SchemaPathsList = ({ schema, schema_id }) => {
   // Organize paths by tag
   const pathsByTag = React.useMemo(() => {
     const byTag = {};
@@ -60,12 +71,19 @@ export const SchemaPathsList = ({ schema }) => {
     return byTag;
   }, [schema]);
 
+  console.log("::Schema: ", schema);
+
   return (
     <Box>
       {Object.keys(pathsByTag).map((tag) => (
         <CollapsibleSection key={tag} title={tag} mb={2}>
           {pathsByTag[tag].map((endpoint, i) => (
-            <SchemaPath key={i} {...endpoint} />
+            <SchemaPath
+              key={i}
+              {...endpoint}
+              schema_id={schema_id}
+              server={schema.servers[0]?.url}
+            />
           ))}
         </CollapsibleSection>
       ))}
