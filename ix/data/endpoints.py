4a2df929071dcd17fa0315d726e626b58eed8a0d
Peter Krenesky <kreneskyp@gmail.com>
2024-01-21 19:00:40 -0800

    Tweaking filter options for get_schemas endpoint

--- ix/data/endpoints.py
+++ ix/data/endpoints.py
@@ -54,7 +54,7 @@ async def get_schema(schema_id: UUID, user: User = Depends(get_request_user)):
 async def get_schemas(
     limit: int = 10,
     offset: int = 0,
-    type: Optional[Literal["openapi", "json"]] = None,
+    type: Literal["openapi", "json"] = "json",
     user: User = Depends(get_request_user),
 ):
     query = Schema.filtered_owners(user)
