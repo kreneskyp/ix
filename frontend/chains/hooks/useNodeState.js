610af8fd5004857e596f9c5b3af2510e24ed7369
Peter Krenesky <kreneskyp@gmail.com>
2024-01-27 12:01:14 -0800

    Convert useNodeState to use useTabDataObject.js

--- frontend/chains/hooks/useNodeState.js
+++ frontend/chains/hooks/useNodeState.js
@@ -1,43 +1,21 @@
-import React from "react";
-import { useTabDataField } from "chains/hooks/useTabDataField";
+import { useTabDataObject } from "chains/hooks/useTabDataObject";
 
 /**
  * Central state for nodes in the editor. All components that need to
  * read or write node state should use this hook.
  */
 export const useNodeState = (tabState) => {
-  const [nodes, setNodes] = useTabDataField(
-    tabState.active,
-    tabState.setActive,
-    "nodes",
-    {
-      node: null,
-      connector: null,
-    }
-  );
+  const {
+    items: nodes,
+    setItems: setNodes,
+    setItem: setNode,
+    deleteItem: deleteNode,
+  } = useTabDataObject(tabState, "nodes");
 
-  const setNode = React.useCallback(
-    (node) => {
-      setNodes((prev) => {
-        // HAX: reject updates from other tabs. This protects against hooks that
-        // aren't updating correctly when switching tabs. This only treats the
-        // symptom, not the cause. But it prevents data corruption of nodes
-        // leaking to other tabs.
-        if (node.chain_id !== tabState.active.chain_id) {
-          return prev;
-        }
-        return { ...prev, [node.id]: node };
-      });
-    },
-    [tabState?.active?.chain_id, setNodes]
-  );
-
-  return React.useMemo(
-    () => ({
-      nodes,
-      setNodes,
-      setNode,
-    }),
-    [nodes, setNodes, setNode]
-  );
+  return {
+    nodes,
+    setNodes,
+    setNode,
+    deleteNode,
+  };
 };
