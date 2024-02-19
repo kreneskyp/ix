25b3a8b24ad5edbf7e8f012c63f4f68e9c76c8fc
Peter Krenesky <kreneskyp@gmail.com>
2024-02-18 17:23:04 -0800

    ExecutionDetail updated to use CodeEditor to render input, output, and error messages

--- frontend/chains/editor/run_log/ExecutionDetail.js
+++ frontend/chains/editor/run_log/ExecutionDetail.js
@@ -1,22 +1,8 @@
 import React from "react";
 import { Box, Heading, VStack } from "@chakra-ui/react";
-import { useColorMode } from "@chakra-ui/color-mode";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import {
-  stackoverflowDark,
-  stackoverflowLight,
-} from "react-syntax-highlighter/dist/cjs/styles/hljs";
-
-import { SCROLLBAR_CSS } from "site/css";
+import CodeEditor from "components/code_editor/CodeEditor";
 
 export const ExecutionDetail = ({ execution }) => {
-  const { colorMode } = useColorMode();
-  const isLight = colorMode === "light";
-  const syntaxTheme = isLight ? stackoverflowLight : stackoverflowDark;
-  const messageStyle = isLight
-    ? { color: "#222", bg: "#f6f6f6" }
-    : { color: "white", bg: "#1c1b1b" };
-
   const formattedInputs =
     typeof execution?.inputs === "string"
       ? execution?.inputs
@@ -26,12 +12,6 @@ export const ExecutionDetail = ({ execution }) => {
       ? execution?.outputs
       : JSON.stringify(execution?.outputs, null, 4);
 
-  const syntaxStyle = {
-    fontSize: "12px",
-    maxWidth: "900px",
-    margniRight: "5px",
-  };
-
   return (
     <Box pl={4} pt={1} height={"100%"}>
       <VStack alignItems={"start"} spacing={3}>
@@ -39,35 +19,37 @@ export const ExecutionDetail = ({ execution }) => {
           <Heading size={"sm"} mb={1}>
             Input
           </Heading>
-          <SyntaxHighlighter
-            customStyle={syntaxStyle}
-            language="json"
-            style={syntaxTheme}
-            wrapLines={true}
-          >
-            {formattedInputs}
-          </SyntaxHighlighter>
+          <Box mr={10}>
+            <CodeEditor
+              key={execution?.id}
+              language="json"
+              value={formattedInputs}
+            />
+          </Box>
         </Box>
         <Box width={"100%"}>
           <Heading size={"sm"} mb={1}>
             Output
           </Heading>
-          <SyntaxHighlighter
-            customStyle={syntaxStyle}
-            language="json"
-            style={syntaxTheme}
-            wrapLines={true}
-          >
-            {formattedOutputs}
-          </SyntaxHighlighter>
+          <Box mr={10}>
+            <CodeEditor
+              key={execution?.id}
+              language="json"
+              value={formattedOutputs}
+            />
+          </Box>
         </Box>
         {execution?.message && (
           <Box width={"100%"}>
             <Heading size={"sm"} mb={1}>
               Message
             </Heading>
-            <Box {...messageStyle} minW={300} minH={100} p={2}>
-              <code>{execution?.message || "<no message>"}</code>
+            <Box mr={10}>
+              <CodeEditor
+                key={execution?.id}
+                language="json"
+                value={execution?.message || "<no message>"}
+              />
             </Box>
           </Box>
         )}
