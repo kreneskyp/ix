9edea680e6c2caf3a423ff3cb3c2c8d0390f419e
Peter Krenesky <kreneskyp@gmail.com>
2024-03-03 14:56:11 -0800

    Updating UpdateComponentClassPath so it wont delete existing nodes (was cascading from the NoedType delete)

--- ix/utils/django/migrations.py
+++ ix/utils/django/migrations.py
@@ -16,11 +16,22 @@ class UpdateComponentClassPath(migrations.RunPython):
         NodeType = apps.get_model("chains", "NodeType")
         ChainNode = apps.get_model("chains", "ChainNode")
         for existing, new in self.changes:
-            ChainNode.objects.filter(class_path=existing).update(class_path=new)
+            try:
+                old_type = NodeType.objects.get(class_path=existing)
+            except NodeType.DoesNotExist:
+                old_type = None
 
-            if NodeType.objects.filter(class_path=existing).exists():
+            try:
+                new_type = NodeType.objects.get(class_path=new)
+            except NodeType.DoesNotExist:
+                # new node_type doesn't exist in test envs
+                return
+
+            # point all nodes at the new type
+            ChainNode.objects.filter(class_path=existing).update(
+                class_path=new, node_type_id=new_type.id
+            )
+
+            if old_type:
                 NodeType.objects.filter(class_path=existing).delete()
-                print(f"Deleted {existing} from NodeType")
-            else:
-                NodeType.objects.filter(class_path=existing).update(class_path=new)
             print(f"Migrated {existing} to {new}")
